d415c13dad3f5c1dccdcb38a57cab2f814f024a3
Marc Rousavy <me@mrousavy.com>
2024-06-24 18:34:07 +0200

    Update index.ts

--- packages/nitro-codegen/src/index.ts
+++ packages/nitro-codegen/src/index.ts
@@ -1,4 +1,4 @@
-import { Project, ts } from "ts-morph"
+import { Node, Project, ts } from "ts-morph"
 
 const project = new Project({ })
 const file = project.addSourceFileAtPath('./src/Person.nitro.ts')
@@ -17,27 +17,58 @@ for (const module of interfaces) {
   if (identifier == null) throw new Error("Interface name cannot be null!")
   const name = identifier.getText()
 
+
+  function getTypeOfChild(child: Node<ts.Node>): ts.SyntaxKind {
+    return child.getLastChildOrThrow().getKind()
+  }
+
+  const cppProperties: string[] = []
+
   const properties = module.getChildrenOfKind(ts.SyntaxKind.PropertySignature).filter(p => p.getFirstChildByKind(ts.SyntaxKind.FunctionType) == null)
-  const methods: string[] = []
   for (const prop of properties) {
     const name = prop.getLastChildByKindOrThrow(ts.SyntaxKind.Identifier).getText()
     const isReadonly = prop.getFirstChildByKind(ts.SyntaxKind.ReadonlyKeyword) != null
-    const type = prop.getChildAtIndex(1)
+    const type = getTypeOfChild(prop)
     // @ts-expect-error
-    const cppType = typeMap[type.getKind()]
+    const cppType = typeMap[type]
 
     const capitalizedName = name.charAt(0).toUpperCase() + name.slice(1)
-    methods.push(`virtual ${cppType} get${capitalizedName}() = 0;`)
+    cppProperties.push(`virtual ${cppType} get${capitalizedName}() = 0;`)
 
     if (!isReadonly) {
-      methods.push(`virtual void set${capitalizedName}(${cppType} value) = 0;`)
+      cppProperties.push(`virtual void set${capitalizedName}(${cppType} value) = 0;`)
     }
+  }
+
+
+  const cppMethods: string[] = []
+
+  const functions = module.getChildrenOfKind(ts.SyntaxKind.MethodSignature)
+  for (const func of functions) {
+    const name = func.getLastChildByKindOrThrow(ts.SyntaxKind.Identifier).getText()
+    const returnType = getTypeOfChild(func)
+    // @ts-expect-error
+    const returnTypeCpp = typeMap[returnType]
+    const parameters = func.getChildrenOfKind(ts.SyntaxKind.Parameter).map((p) => {
+      const name = p.getFirstChildByKindOrThrow(ts.SyntaxKind.Identifier).getText()
+      const type = getTypeOfChild(p)
+      // @ts-expect-error
+      const cppType = typeMap[type]
+      return `${cppType} ${name}`
+    })
+
+    cppMethods.push(`virtual ${returnTypeCpp} ${name}(${parameters.join(', ')}) = 0;`)
   }
 
   let cppCode = `
 class ${name}: public HybridObject {
   public:
-    ${methods.join('\n    ')}
+    // Properties
+    ${cppProperties.join('\n    ')}
+
+  public:
+    // Methods
+    ${cppMethods.join('\n    ')}
 };
     `
 
