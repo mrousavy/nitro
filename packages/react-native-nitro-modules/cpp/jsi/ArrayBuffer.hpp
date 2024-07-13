d3f6b042ea38175cd5929242229d2f0610eb8af6
Marc Rousavy <me@mrousavy.com>
2024-07-14 00:51:31 +0200

    Update ArrayBuffer.hpp

--- packages/react-native-nitro-modules/cpp/jsi/ArrayBuffer.hpp
+++ packages/react-native-nitro-modules/cpp/jsi/ArrayBuffer.hpp
@@ -13,18 +13,23 @@ namespace margelo {
 
 using namespace facebook;
 
+/**
+ * Represents a raw byte buffer that can be read from-, and
+ * written to- from both JavaScript and C++.
+ * `ArrayBuffer` is not thread-safe and does not lock multi-thread access.
+ */
 class ArrayBuffer: public jsi::MutableBuffer {
 public:
   explicit ArrayBuffer(uint8_t* data, size_t size): _data(data), _size(size) { }
-  
+
   uint8_t* data() override {
     return _data;
   }
-  
+
   size_t size() const override {
     return _size;
   }
-  
+
 private:
   uint8_t* _data;
   size_t _size;
