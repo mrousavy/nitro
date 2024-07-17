818ec1a014cfb789f1904e9cd58c47d07df5d416
Marc Rousavy <me@mrousavy.com>
2024-07-17 22:14:44 +0200

    Create `NitroModules.get`

--- packages/react-native-nitro-modules/src/index.ts
+++ packages/react-native-nitro-modules/src/index.ts
@@ -1,4 +1,14 @@
-export { NitroModules } from './NativeNitro'
-
 export * from './createTestObject'
 export * from './HybridObject'
+
+type ExtractCreateMethods<T> = {
+  [K in keyof T as K extends `create${infer Rest}`
+    ? `init${Rest}`
+    : never]: T[K]
+}
+
+export const NitroModules = {
+  get<T>(): ExtractCreateMethods<T> {
+    return null as unknown as ExtractCreateMethods<T>
+  },
+}
