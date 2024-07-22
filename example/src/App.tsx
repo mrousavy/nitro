a4b2353217ee6a026c7dec34e916436b0fe3d4b7
Marc Rousavy <me@mrousavy.com>
2024-07-22 23:54:31 +0200

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -5,30 +5,23 @@ import { ImageConstructors } from 'react-native-nitro-image'
 
 
 export default function App() {
-  React.useEffect(() => {
+  const image = React.useMemo(() => {
     console.log('Loading image...')
-    let image = ImageConstructors.loadImageFromSystemName('heart.fill')
+    const i = ImageConstructors.loadImageFromSystemName('heart.fill')
     console.log('Image loaded!')
-    console.log(`Image is ${image.size.width}x${image.size.height}`)
+    console.log(`Image is ${i.size.width}x${i.size.height}`)
+    return i
+  }, [])
 
+  React.useEffect(() => {
     image.saveToFile('some path', (path) => {
       console.log('saved to ' + path + '!')
     })
-
   }, [])
-  // React.useEffect(() => {
-  //   const timeout = setTimeout(async () => {
-  //     await runCppHybridObjectTests()
-  //     await runSwiftHybridObjectTests()
-  //   }, 1000)
-  //   return () => {
-  //     clearTimeout(timeout)
-  //   }
-  // }, [])
 
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text>Image is {image.size.width.toFixed(0)}x{image.size.height.toFixed(0)}!</Text>
     </View>
   );
 }
