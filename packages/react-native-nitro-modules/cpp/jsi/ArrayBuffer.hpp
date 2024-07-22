8e4f784147fee47841b0a4920cfb9552dd7f93b5
Marc Rousavy <me@mrousavy.com>
2024-07-22 23:52:34 +0200

    fix: Fix stupid fucking idiotic dumb error

--- packages/react-native-nitro-modules/cpp/jsi/ArrayBuffer.hpp
+++ packages/react-native-nitro-modules/cpp/jsi/ArrayBuffer.hpp
@@ -37,17 +37,18 @@ public:
   
   ~ArrayBuffer() {
   }
-
+  
   uint8_t* data() override {
     return _data;
   }
-
+  
   size_t size() const override {
     return _size;
   }
-
+  
 private:
   uint8_t* _data;
   size_t _size;
 };
 
+} // namespace margelo::nitro
