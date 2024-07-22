c6c349d63582e50d7bdfd1ad072533fae52efe9c
Marc Rousavy <me@mrousavy.com>
2024-07-23 01:40:41 +0200

    Update HybridObjectSpec.swift

--- packages/react-native-nitro-modules/ios/core/HybridObjectSpec.swift
+++ packages/react-native-nitro-modules/ios/core/HybridObjectSpec.swift
@@ -14,13 +14,28 @@ public protocol HybridObjectSpec {
   /**
    * Holds the C++ HybridObject and it's context.
    * Use the default initializer in your implementation, C++ will set and get this value.
+   *
+   * @example
+   * ```swift
+   * var hybridContext = margelo.nitro.HybridContext()
+   * ```
    */
   var hybridContext: margelo.nitro.HybridContext { get set }
+  
   /**
-   * Get the memory size of the instance, in bytes.
+   * Get the memory size of the Swift instance (plus any external heap allocations),
+   * in bytes.
    *
    * Override this to allow tracking heap allocations such as buffers or images,
    * which will help the JS GC be more efficient in deleting large unused objects.
+   *
+   * @example
+   * ```swift
+   * var memorySize: Int {
+   *   let imageSize = self.uiImage.bytesPerRow * self.uiImage.height
+   *   return getSizeOf(self) + imageSize
+   * }
+   * ```
    */
   var memorySize: Int { get }
 }
