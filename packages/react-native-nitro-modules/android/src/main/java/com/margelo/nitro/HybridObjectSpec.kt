d6add47da96e4d556166a8c1222f0e388a90b6a4
Marc Rousavy <me@mrousavy.com>
2024-07-26 20:23:23 +0200

    Add `memorySize`

--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/HybridObjectSpec.kt
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/HybridObjectSpec.kt
@@ -1,4 +1,24 @@
 package com.margelo.nitro
 
+/**
+ * A base protocol for all Kotlin-based Hybrid Objects.
+ */
 interface HybridObjectSpec {
+    /**
+     * Get the memory size of the Kotlin instance (plus any external heap allocations),
+     * in bytes.
+     *
+     * Override this to allow tracking heap allocations such as buffers or images,
+     * which will help the JS GC be more efficient in deleting large unused objects.
+     *
+     * @example
+     * ```kotlin
+     * val memorySize: Int
+     *   get() {
+     *     val imageSize = this.bitmap.bytesPerRow * this.bitmap.height
+     *     return getSizeOf(this) + imageSize
+     *   }
+     * ```
+     */
+    val memorySize: Int
 }
