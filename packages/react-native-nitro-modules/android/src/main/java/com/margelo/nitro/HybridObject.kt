20e771a2bc8f006e9b6e01a26f5b8bb9f64272c5
Marc Rousavy <me@mrousavy.com>
2024-07-27 15:02:31 +0200

    Inherit `HybridObject`

--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/HybridObject.kt
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/HybridObject.kt
@@ -1,18 +1,29 @@
 package com.margelo.nitro
 
 import androidx.annotation.Keep
-import com.facebook.jni.HybridData
 import com.facebook.proguard.annotations.DoNotStrip
 
-@Suppress("KotlinJniMissingFunction")
-abstract class HybridObject: HybridObjectSpec {
-    @DoNotStrip
-    @Keep
-    private val mHybridData: HybridData
-
-    init {
-        mHybridData = initHybrid()
-    }
-
-    private external fun initHybrid(): HybridData
+/**
+ * A base interface for all Kotlin-based HybridObjects.
+ */
+interface HybridObject {
+    /**
+     * Get the memory size of the Kotlin instance (plus any external heap allocations),
+     * in bytes.
+     *
+     * Override this to allow tracking heap allocations such as buffers or images,
+     * which will help the JS GC be more efficient in deleting large unused objects.
+     *
+     * @example
+     * ```kotlin
+     * val memorySize: ULong
+     *   get() {
+     *     val imageSize = this.bitmap.bytesPerRow * this.bitmap.height
+     *     return getSizeOf(this) + imageSize
+     *   }
+     * ```
+     */
+    @get:DoNotStrip
+    @get:Keep
+    val memorySize: ULong
 }
