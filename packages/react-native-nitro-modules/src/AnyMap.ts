6be76223ecffabd0e737cc182570d469ea448ab9
Marc Rousavy <me@mrousavy.com>
2024-07-30 17:43:34 +0200

    Update AnyMap.ts

--- packages/react-native-nitro-modules/src/AnyMap.ts
+++ packages/react-native-nitro-modules/src/AnyMap.ts
@@ -1,3 +1,21 @@
-export type ValueType = string | number | boolean | bigint | ValueType[]
+/**
+ * Represents a single value inside an untyped map.
+ */
+export type ValueType =
+  | string
+  | number
+  | boolean
+  | bigint
+  | ValueType[]
+  | { [k: string]: ValueType }
 
+/**
+ * Represents an untyped map, similar to a JSON structure.
+ * Supported types:
+ * - Primitives (`string`, `number`, `boolean`, `bigint`)
+ * - Arrays of primitives (`ValueType[]`)
+ * - Objects of primitives (`Record<string, ValueType>`)
+ * - Arrays of arrays or objects
+ * - Objects of arrays or objects
+ */
 export type AnyMap = Record<string, ValueType>
