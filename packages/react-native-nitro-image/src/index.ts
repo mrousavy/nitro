a8fc494039ff5a73bd78fb015e51016f7d6bf221
Marc Rousavy <me@mrousavy.com>
2024-07-30 13:00:31 +0200

    Update index.ts

--- packages/react-native-nitro-image/src/index.ts
+++ packages/react-native-nitro-image/src/index.ts
@@ -1,5 +1,6 @@
 import { NitroModules } from 'react-native-nitro-modules'
 import type { ImageFactory } from './specs/ImageFactory.nitro'
+import type { TestObject } from './specs/HybridTest.nitro'
 
 export * from './specs/Image.nitro'
 export * from './specs/ImageFactory.nitro'
@@ -8,3 +9,8 @@ export * from './specs/ImageFactory.nitro'
  * Constructors for creating instances of `Image`.
  */
 export const ImageConstructors = NitroModules.get<ImageFactory>('ImageFactory')
+
+/**
+ * The Hybrid Test Object
+ */
+export const HybridTestObject = NitroModules.get<TestObject>('TestObject')
