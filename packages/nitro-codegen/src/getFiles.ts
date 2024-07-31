633b45c9923cc2bbdf8ff5e21d62e913ca0b71c3
Marc Rousavy <me@mrousavy.com>
2024-07-31 23:58:53 +0200

    Update getFiles.ts

--- packages/nitro-codegen/src/getFiles.ts
+++ packages/nitro-codegen/src/getFiles.ts
@@ -2,18 +2,25 @@ import { promises as fs } from 'fs'
 import path from 'path'
 
 export async function getFiles(directory: string): Promise<string[]> {
-  const files = await fs.readdir(directory, {
-    recursive: true,
-  })
+  try {
+    const files = await fs.readdir(directory, {
+      recursive: true,
+      withFileTypes: true,
+    })
 
-  const filtered = files.map(async (f) => {
-    const file = path.join(directory, f)
-    const stat = await fs.stat(file)
-    if (stat.isDirectory()) {
-      return undefined
+    return files.filter((f) => f.isFile()).map((f) => path.join(f.path, f.name))
+  } catch (error) {
+    if (
+      typeof error === 'object' &&
+      error != null &&
+      'code' in error &&
+      error.code === 'ENOENT'
+    ) {
+      // directory does not exist
+      return []
+    } else {
+      // different error
+      throw error
     }
-    return file
-  })
-  const all = await Promise.all(filtered)
-  return all.filter((v) => v != null)
+  }
 }
