9e9bec57fa37a6fbcbe05f70c6a4a55dfa3452c9
Marc Rousavy <me@mrousavy.com>
2024-08-04 22:51:59 +0200

    Update NonDefaultArgsCount.hpp

--- packages/react-native-nitro-modules/cpp/templates/NonDefaultArgsCount.hpp
+++ packages/react-native-nitro-modules/cpp/templates/NonDefaultArgsCount.hpp
@@ -20,30 +20,31 @@ struct is_optional<std::optional<T>> : std::true_type {};
 
 // Base case: No arguments, so the count is 0
 template <typename... Args>
-struct CountTrailingOptionals;
+struct count_trailing_optionals;
 
 // Specialization for empty parameter pack
 template <>
-struct CountTrailingOptionals<> {
+struct count_trailing_optionals<> {
     static constexpr size_t value = 0;
 };
 
 // Recursive case: Check the last type, then process the rest
 template <typename Last>
-struct CountTrailingOptionals<Last> {
+struct count_trailing_optionals<Last> {
     static constexpr size_t value = is_optional<Last>::value;
 };
 
+// Counts the number of consecutive trailing std::optionals a given type pack contains.
 template <typename First, typename... Rest>
-struct CountTrailingOptionals<First, Rest...> {
+struct count_trailing_optionals<First, Rest...> {
 private:
-    static constexpr size_t rest_value = CountTrailingOptionals<Rest...>::value;
+    static constexpr size_t rest_value = count_trailing_optionals<Rest...>::value;
     static constexpr bool rest_all_optionals = rest_value == sizeof...(Rest);
 public:
     static constexpr size_t value = rest_all_optionals && is_optional<First>::value ? rest_value + 1 : rest_value;
 };
 
 template<typename... Args>
-constexpr size_t count_trailing_optionals_v = CountTrailingOptionals<Args...>::value;
+constexpr size_t count_trailing_optionals_v = count_trailing_optionals<Args...>::value;
 
 } // namespace margelo::nitro
