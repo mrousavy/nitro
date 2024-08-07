9b0e8320bfd6a5b3e14dca70fcbf50915ff935ea
Marc Rousavy <me@mrousavy.com>
2024-08-07 19:26:15 +0200

    feat: Freeze prototypes

--- packages/react-native-nitro-modules/cpp/prototype/HybridObjectPrototype.cpp
+++ packages/react-native-nitro-modules/cpp/prototype/HybridObjectPrototype.cpp
@@ -23,10 +23,14 @@ void HybridObjectPrototype::ensureInitialized() {
 }
 
 jsi::Object HybridObjectPrototype::createPrototype(jsi::Runtime& runtime, Prototype* prototype) {
+  // 0. Check if we're at the highest level of our prototype chain
   if (prototype == nullptr) {
-    // There is no prototype - we just have an empty base.
+    // There is no prototype - we just have an empty Object base - so `Object.create({})`
     return jsi::Object(runtime);
   }
+  
+  // 1. Try looking for the given prototype in cache.
+  //    If we find it in cache, we can create instances faster and skip creating the prototype from scratch!
   auto& prototypeCache = _prototypeCache[&runtime];
   auto cachedPrototype = prototypeCache.find(prototype->instanceTypeId);
   if (cachedPrototype != prototypeCache.end()) {
@@ -35,24 +39,25 @@ jsi::Object HybridObjectPrototype::createPrototype(jsi::Runtime& runtime, Protot
     return jsi::Value(runtime, *cachedObject).getObject(runtime);
   }
 
+  // 2. We didn't find the given prototype in cache (either it's a new prototype, or a new runtime),
+  //    so we need to create it. First, we need some helper methods from JS
   Logger::log(TAG, "Creating new prototype for C++ instance type \"%s\"...", prototype->instanceTypeId.name());
-
-  // 0. Get some helper JS methods from global
   jsi::Object objectConstructor = runtime.global().getPropertyAsObject(runtime, "Object");
   jsi::Function objectCreate = objectConstructor.getPropertyAsFunction(runtime, "create");
   jsi::Function objectDefineProperty = objectConstructor.getPropertyAsFunction(runtime, "defineProperty");
+  jsi::Function objectFreeze = objectConstructor.getPropertyAsFunction(runtime, "freeze");
 
-  // 1. Create an empty JS Object, inheriting from the base prototype
+  // 3. Create an empty JS Object, inheriting from the base prototype (recursively!)
   jsi::Object object = objectCreate.call(runtime, createPrototype(runtime, prototype->base)).getObject(runtime);
 
-  // 2. Add all Hybrid Methods to it
+  // 4. Add all Hybrid Methods to it
   for (const auto& method : prototype->methods) {
     object.setProperty(runtime, method.first.c_str(),
                        jsi::Function::createFromHostFunction(runtime, jsi::PropNameID::forUtf8(runtime, method.first),
                                                              method.second.parameterCount, method.second.function));
   }
 
-  // 3. Add all properties (getter + setter) to it using defineProperty
+  // 5. Add all properties (getter + setter) to it using defineProperty
   for (const auto& getter : prototype->getters) {
     jsi::Object property(runtime);
     property.setProperty(runtime, "configurable", false);
@@ -71,13 +76,16 @@ jsi::Object HybridObjectPrototype::createPrototype(jsi::Runtime& runtime, Protot
                               /* propName */ jsi::String::createFromUtf8(runtime, getter.first.c_str()),
                               /* descriptorObj */ property);
   }
+  
+  // 6. Freeze the object now so no one accidentally modifies it in JS
+  objectFreeze.call(runtime, object);
 
-  // 4. Throw it into our cache
+  // 7. Throw it into our cache so the next lookup can be cached and therefore faster
   JSICacheReference jsiCache = JSICache::getOrCreateCache(runtime);
   OwningReference<jsi::Object> cachedObject = jsiCache.makeShared(std::move(object));
   prototypeCache.emplace(prototype->instanceTypeId, cachedObject);
 
-  // 5. Return it!
+  // 8. Return it!
   return jsi::Value(runtime, *cachedObject).getObject(runtime);
 }
 
