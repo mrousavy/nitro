a92fc20590ea39afd5c72eada9c809733ede3ce9
Marc Rousavy <me@mrousavy.com>
2024-08-09 13:59:42 +0200

    Update logPrototypeChain.ts

--- example/src/logPrototypeChain.ts
+++ example/src/logPrototypeChain.ts
@@ -1,12 +1,13 @@
 import type { HybridObject } from 'react-native-nitro-modules'
 
 export function logPrototypeChain(type: HybridObject): void {
+  console.log(`Prototype chain of ${type.name}:`)
   let object = type
-  let indentation = ''
+  let indentation = '  '
   while (object != null) {
-    if (indentation === '') console.log(object.__type)
-    else console.log(`${indentation}∟ ${object.__type ?? '{}'}`)
-    indentation += ' '
+    const prefix = object === type ? indentation : `${indentation}∟ `
+    console.log(`${prefix}${object.__type ?? '{}'}`)
+    indentation += '   '
     object = Object.getPrototypeOf(object)
   }
 }
