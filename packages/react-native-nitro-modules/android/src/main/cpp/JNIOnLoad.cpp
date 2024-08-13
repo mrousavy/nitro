d57c1556f2b7137ac588db494909efc07c734917
Marc Rousavy <me@mrousavy.com>
2024-08-14 00:01:41 +0200

    fix: Fix Android build/JNI setup (#27)
    * fix: Android cmake order
    
    * chore: fix CMake GLOB use recursive
    
    * chore: fix Android JNI setup
    
    ---------
    
    Co-authored-by: Christoph Pader <christoph@margelo.io>

--- packages/react-native-nitro-modules/android/src/main/cpp/JNIOnLoad.cpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/JNIOnLoad.cpp
@@ -2,16 +2,17 @@
 
 #include "JHybridObjectRegistry.hpp"
 #include "RegisterNativeNitroModules.hpp"
+#include <fbjni/fbjni.h>
 #include <jni.h>
 
 using namespace margelo::nitro;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  // 1. Initialize the Nitro JSI Turbo Module
-  RegisterNativeNitroModules::registerNativeNitroModules();
+  return facebook::jni::initialize(vm, [] {
+    // 1. Initialize the Nitro JSI Turbo Module
+    RegisterNativeNitroModules::registerNativeNitroModules();
 
-  // 2. Initialize all Java bindings
-  JHybridObjectRegistry::registerNatives();
-
-  return JNI_VERSION_1_2;
+    // 2. Initialize all Java bindings
+    JHybridObjectRegistry::registerNatives();
+  });
 }
--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/NitroModulesPackage.java
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/NitroModulesPackage.java
@@ -1,5 +1,6 @@
 package com.margelo.nitro;
 
+import android.util.Log;
 import androidx.annotation.Nullable;
 
 import com.facebook.react.bridge.NativeModule;
@@ -10,6 +11,17 @@ import com.facebook.react.TurboReactPackage;
 import java.util.HashMap;
 
 public class NitroModulesPackage extends TurboReactPackage {
+  private static final String TAG = "NitroModules";
+  static {
+    try {
+      Log.i(TAG, "Loading NitroModules C++ library...");
+      System.loadLibrary("NitroModules");
+      Log.i(TAG, "Successfully loaded NitroModules C++ library!");
+    } catch (Throwable e) {
+      Log.e(TAG, "Failed to load NitroModules C++ library! Is it properly installed and linked?", e);
+      throw e;
+    }
+  }
 
   @Nullable
   @Override
