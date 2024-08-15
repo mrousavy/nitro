33c3cd92ceb53f950ce2e1d34ce185c74369e690
Marc Rousavy <me@mrousavy.com>
2024-08-15 17:42:31 +0200

    Create `PromiseHolder`

--- packages/react-native-nitro-modules/ios/core/PromiseHolder.hpp
+++ packages/react-native-nitro-modules/ios/core/PromiseHolder.hpp
@@ -8,7 +8,8 @@
 #pragma once
 
 #include <future>
-#include "NitroDefines.hpp"
+#include <string>
+#include <swift/bridging>
 
 namespace margelo::nitro {
 
@@ -16,8 +17,42 @@ namespace margelo::nitro {
  * Holds a `std::promise` that can be accessed from Swift using proper ref management.
  */
 class PromiseHolder {
-public:
+  using T = int;
   
+public:
+  /**
+   * Create a new PromiseHolder (and a new `std::promise<T>`).
+   */
+  explicit PromiseHolder() {
+    _promise = std::make_shared<std::promise<T>>();
+  }
+  
+  /**
+   * Resolve the underlying `std::promise<T>` with `T`.
+   */
+  void resolve(const T& result) {
+    _promise->set_value(result);
+  }
+  
+  /**
+   * Reject the underlying `std::promise<T>` with the given message.
+   */
+  void reject(const std::string& message) {
+    try {
+      throw std::runtime_error(message);
+    } catch (...) {
+      _promise->set_exception(std::current_exception());
+    }
+  }
+  
+  /**
+   * Get the `std::future<T>` of the underlying `std::promise<T>`.
+   * This can only be called once.
+   */
+  std::future<T> getFuture() { return _promise->get_future(); }
+  
+private:
+  std::shared_ptr<std::promise<T>> _promise;
 };
 
 } // namespace margelo::nitro
