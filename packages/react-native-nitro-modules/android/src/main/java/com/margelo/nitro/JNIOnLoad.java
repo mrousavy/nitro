90b25e242650379992f057dcc0311dcf9a1fcf36
Marc Rousavy <me@mrousavy.com>
2024-08-16 14:16:21 +0200

    Update JNIOnLoad.java

--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/JNIOnLoad.java
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/JNIOnLoad.java
@@ -5,7 +5,14 @@ import android.util.Log;
 public class JNIOnLoad {
     private static final String TAG = "NitroModules";
     private static boolean isInitialized = false;
-    public static void initializeNativeNitro() {
+
+    /**
+     * Initializes the C++ (JNI) part of Nitro Modules.
+     * This function should be called in a `static` block everytime the Java part
+     * interacts with the native C++ (JNI) part of Nitro Modules.
+     * If Native Nitro is already initialized, this function will do nothing.
+     */
+    public synchronized static void initializeNativeNitro() {
         if (isInitialized) return;
         try {
             Log.i(TAG, "Loading NitroModules C++ library...");
