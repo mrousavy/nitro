2d1a4b31d2fd4815a3f9a5804019ae03f92ffc54
Marc Rousavy <me@mrousavy.com>
2024-08-18 11:06:32 +0200

    Update JHybridObject.hpp

--- packages/react-native-nitro-modules/android/src/main/cpp/core/JHybridObject.hpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/core/JHybridObject.hpp
@@ -14,14 +14,16 @@ namespace margelo::nitro {
 
 using namespace facebook;
 
+/**
+ * Represents the Java `HybridObject` instance.
+ * HybridData is passed up from inherited members, so this acts like a base class
+ * and has to be inherited as "virtual" in C++ to properly avoid creating multiple `HybridObject` instances.
+ */
 struct JHybridObject : public jni::HybridClass<JHybridObject>, public virtual HybridObject {
 public:
   static auto constexpr kJavaDescriptor = "Lcom/margelo/nitro/HybridObject;";
 
-  explicit JHybridObject(const char* name): HybridObject(name) { }
-
   virtual ~JHybridObject() = default;
-
 private:
   friend HybridBase;
 };
