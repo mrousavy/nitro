d9556162d1f9d9fa223d687160aff132290afe58
Marc Rousavy <me@mrousavy.com>
2024-08-28 15:30:50 +0200

    Update App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { HybridObjectTestsScreen } from './screens/HybridObjectTestsScreen'
 
+const thisshouldbreak = true
+
 export default function App() {
   return (
     <SafeAreaProvider>
