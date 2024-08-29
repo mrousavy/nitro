cacc1b1c64fc0445d07da96b80c12e468149b5d2
Marc Rousavy <me@mrousavy.com>
2024-08-29 21:32:59 +0200

    Add `docs` to the monorepo's workspaces

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -3,20 +3,21 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Nitro Modules',
+  tagline: 'Highly efficient native modules with a statically compiled binding layer to JSI, and typesafe JS bindings',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://mrousavy.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/nitro/',
 
   // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mrousavy',
+  projectName: 'nitro',
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
