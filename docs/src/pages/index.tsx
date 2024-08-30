96228789cefc363fc06462a7860f30a4e0645b4c
Laurids Kern <me@lauridskern.com>
2024-08-30 16:56:19 +0200

    add dynamic background

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -7,24 +7,29 @@ import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
-const Icon = require('@site/static/img/nos.png').default
-
+const Icon = require('@site/static/img/nos.png').default;
+const BgImage = require('@site/static/img/hero-bg-light.webp').default;
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <img src={Icon} className={styles.heroImage} />
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Getting Started - 5min ⏱️
-          </Link>
+    <header className={clsx('', styles.heroBanner)}>
+      <div className={styles.heroContainer}>
+        <img className={styles.heroBgImage} src={BgImage} />
+        <div className={styles.bgNoise} />
+        <div className={styles.bgPattern} />
+        <div className={styles.heroContentContainer}>
+          <Heading as="h1" className="hero__title">
+            {siteConfig.title}
+          </Heading>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--secondary button--lg"
+              to="/docs/intro"
+            >
+              Getting Started - 5min ⏱️
+            </Link>
+          </div>
         </div>
       </div>
     </header>
@@ -35,7 +40,8 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title="Welcome to Nitro!"
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
