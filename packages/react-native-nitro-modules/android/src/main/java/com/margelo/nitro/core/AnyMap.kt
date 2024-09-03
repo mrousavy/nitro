57c5c5feb9da9fc87f89cfb98e718f0766862055
Marc Rousavy <me@mrousavy.com>
2024-09-03 20:19:17 +0200

    perf: Make `AnyMap` operations faster by marking them `@CriticalNative`

--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/core/AnyMap.kt
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/core/AnyMap.kt
@@ -3,6 +3,7 @@ package com.margelo.nitro.core
 import androidx.annotation.Keep
 import com.facebook.jni.HybridData
 import com.facebook.proguard.annotations.DoNotStrip
+import dalvik.annotation.optimization.CriticalNative
 
 /**
  * Represents an untyped map of string keys with associated values.
@@ -30,28 +31,45 @@ class AnyMap {
   }
 
 
+  @CriticalNative
   external fun contains(key: String): Boolean
+  @CriticalNative
   external fun remove(key: String)
+  @CriticalNative
   external fun clear()
 
+  @CriticalNative
   external fun isNull(key: String): Boolean
+  @CriticalNative
   external fun isDouble(key: String): Boolean
+  @CriticalNative
   external fun isBoolean(key: String): Boolean
+  @CriticalNative
   external fun isBigInt(key: String): Boolean
+  @CriticalNative
   external fun isString(key: String): Boolean
+  @CriticalNative
   external fun isArray(key: String): Boolean
+  @CriticalNative
   external fun isObject(key: String): Boolean
 
+  @CriticalNative
   external fun getDouble(key: String): Double
+  @CriticalNative
   external fun getBoolean(key: String): Boolean
+  @CriticalNative
   external fun getBigInt(key: String): Long
   external fun getString(key: String): String
   external fun getAnyArray(key: String): AnyArray
   external fun getAnyObject(key: String): AnyObject
 
+  @CriticalNative
   external fun setNull(key: String)
+  @CriticalNative
   external fun setDouble(key: String, value: Double)
+  @CriticalNative
   external fun setBoolean(key: String, value: Boolean)
+  @CriticalNative
   external fun setBigInt(key: String, value: Long)
   external fun setString(key: String, value: String)
   external fun setAnyArray(key: String, value: AnyArray)
