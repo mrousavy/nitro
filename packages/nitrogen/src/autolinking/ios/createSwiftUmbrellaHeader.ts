589cabe781b054b9977dc1729461c363c7793428
Marc Rousavy <me@mrousavy.com>
2024-09-12 17:22:45 +0200

    fix: Fix CI

--- packages/nitrogen/src/autolinking/ios/createSwiftUmbrellaHeader.ts
+++ packages/nitrogen/src/autolinking/ios/createSwiftUmbrellaHeader.ts
@@ -3,7 +3,10 @@ import { getForwardDeclaration } from '../../syntax/c++/getForwardDeclaration.js
 import { includeHeader } from '../../syntax/c++/includeNitroHeader.js'
 import { getAllKnownTypes } from '../../syntax/createType.js'
 import { getHybridObjectName } from '../../syntax/getHybridObjectName.js'
-import { createFileMetadataString, isNotDuplicate } from '../../syntax/helpers.js'
+import {
+  createFileMetadataString,
+  isNotDuplicate,
+} from '../../syntax/helpers.js'
 import type { SourceFile } from '../../syntax/SourceFile.js'
 import { getTypeAs } from '../../syntax/types/getTypeAs.js'
 import { HybridObjectType } from '../../syntax/types/HybridObjectType.js'
