9913cf0e3165cc4328ed30f5ed672c685200a2c8
Marc Rousavy <me@mrousavy.com>
2024-09-14 00:48:21 +0200

    feat: Implement JArrayClass

--- packages/react-native-nitro-modules/android/src/main/cpp/jsi/JSIConverter+JNI.hpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/jsi/JSIConverter+JNI.hpp
@@ -12,6 +12,7 @@ struct JSIConverter;
 
 #include "JSIConverter.hpp"
 #include <fbjni/fbjni.h>
+#include <jni.h>
 #include <jsi/jsi.h>
 #include <type_traits>
 #include <variant>
@@ -20,7 +21,7 @@ namespace margelo::nitro {
 
 using namespace facebook;
 
-// number? <> Double?
+// number? <> JDouble?
 template <>
 struct JSIConverter<jni::JDouble> final {
   static inline jni::local_ref<jni::JDouble> fromJSI(jsi::Runtime&, const jsi::Value& arg) {
@@ -42,7 +43,7 @@ struct JSIConverter<jni::JDouble> final {
   }
 };
 
-// boolean? <> Bool?
+// boolean? <> JBoolean?
 template <>
 struct JSIConverter<jni::JBoolean> final {
   static inline jni::local_ref<jni::JBoolean> fromJSI(jsi::Runtime&, const jsi::Value& arg) {
@@ -64,7 +65,7 @@ struct JSIConverter<jni::JBoolean> final {
   }
 };
 
-// bigint? <> Long?
+// bigint? <> JLong?
 template <>
 struct JSIConverter<jni::JLong> final {
   static inline jni::local_ref<jni::JLong> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
@@ -83,6 +84,50 @@ struct JSIConverter<jni::JLong> final {
   }
   static inline bool canConvert(jsi::Runtime&, const jsi::Value& value) {
     return value.isUndefined() || value.isNull() || value.isBigInt();
+  }
+};
+
+// string <> JString
+template <>
+struct JSIConverter<jni::JString> final {
+  static inline jni::local_ref<jni::JString> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
+    return jni::make_jstring(arg.asString(runtime).utf8(runtime));
+  }
+  static inline jsi::Value toJSI(jsi::Runtime& runtime, const jni::alias_ref<jni::JString>& arg) {
+    return jsi::String::createFromUtf8(runtime, arg->toStdString());
+  }
+  static inline bool canConvert(jsi::Runtime&, const jsi::Value& value) {
+    return value.isString();
+  }
+};
+
+// [] <> JArray
+template <typename T>
+struct JSIConverter<jni::JArrayClass<T>> final {
+  static inline jni::local_ref<jni::JArrayClass<T>> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
+    jsi::Object object = arg.asObject(runtime);
+    jsi::Array array = object.asArray(runtime);
+    size_t size = array.size(runtime);
+    jni::local_ref<jni::JArrayClass<T>> result = jni::JArrayClass<T>::newArray(size);
+    for (size_t i = 0; i < size; i++) {
+      result->setElement(i, JSIConverter<T>::fromJSI(runtime, array.getValueAtIndex(runtime, i)));
+    }
+    return result;
+  }
+  static inline jsi::Value toJSI(jsi::Runtime& runtime, const jni::alias_ref<jni::JArrayClass<T>>& arg) {
+    size_t size = arg->size();
+    jsi::Array array = jsi::Array(runtime, size);
+    for (size_t i = 0; i < size; i++) {
+      array.setValueAtIndex(runtime, i, JSIConverter<T>::toJSI(runtime, arg->getElement(i)));
+    }
+    return array;
+  }
+  static inline bool canConvert(jsi::Runtime& runtime, const jsi::Value& value) {
+    if (!value.isObject()) {
+      return false;
+    }
+    jsi::Object object = value.getObject(runtime);
+    return object.isArray(runtime);
   }
 };
 
