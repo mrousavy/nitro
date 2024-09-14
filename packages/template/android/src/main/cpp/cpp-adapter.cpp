661f99f7e0b86feafddcbebf162c757d0f9318e7
Parsa Nasirimehr <40071952+TheRogue76@users.noreply.github.com>
2024-09-14 15:38:36 +0200

    fix: Fix template cpp-adapter.cpp for Android (#122)
    * fix: template cpp-adapter.cpp for Android
    
    * fix: remove the HybridImageFactory headers that are not mentioned in CMake

--- packages/template/android/src/main/cpp/cpp-adapter.cpp
+++ packages/template/android/src/main/cpp/cpp-adapter.cpp
@@ -2,15 +2,13 @@
 
 #include "JFunc_void_Person.hpp"
 #include "JFunc_void_std__string.hpp"
-#include "JHybridImageFactorySpec.hpp"
-#include "JHybridImageSpec.hpp"
 #include "JHybridKotlinTestObjectSpec.hpp"
 
 #include "HybridTestObject.hpp"
 #include <NitroModules/HybridObjectRegistry.hpp>
 #include <fbjni/fbjni.h>
 
-using namespace margelo::nitro::image;
+using namespace margelo::nitro::<<androidCxxLibName>>;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, [] {
