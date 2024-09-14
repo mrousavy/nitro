926f8999976d6d0ace3fd5f2fdb78cdcfa7ab8dd
Parsa Nasirimehr <40071952+TheRogue76@users.noreply.github.com>
2024-09-14 15:19:00 +0200

    fix: template cpp-adapter.cpp for Android

--- packages/template/android/src/main/cpp/cpp-adapter.cpp
+++ packages/template/android/src/main/cpp/cpp-adapter.cpp
@@ -10,7 +10,7 @@
 #include <NitroModules/HybridObjectRegistry.hpp>
 #include <fbjni/fbjni.h>
 
-using namespace margelo::nitro::image;
+using namespace margelo::nitro::<<androidCxxLibName>>;
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, [] {
