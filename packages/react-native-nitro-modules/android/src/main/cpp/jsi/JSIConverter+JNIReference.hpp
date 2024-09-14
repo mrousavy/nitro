dfe0f7454915c3caf43547b3fe85ba6e3b087e15
Marc Rousavy <me@mrousavy.com>
2024-09-14 14:56:41 +0200

    Add global_ref

--- packages/react-native-nitro-modules/android/src/main/cpp/jsi/JSIConverter+JNIReference.hpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/jsi/JSIConverter+JNIReference.hpp
@@ -19,6 +19,7 @@ namespace margelo::nitro {
 
 using namespace facebook;
 
+// T <> jni::alias_ref<T>
 template <typename T>
 struct JSIConverter<jni::alias_ref<T>> final {
   static inline jni::alias_ref<T> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
@@ -32,10 +33,25 @@ struct JSIConverter<jni::alias_ref<T>> final {
   }
 };
 
+// T <> jni::local_ref<T>
 template <typename T>
 struct JSIConverter<jni::local_ref<T>> final {
   static inline jni::alias_ref<T> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
     return JSIConverter<T>::fromJSI(runtime, arg);
+  }
+  static inline jsi::Value toJSI(jsi::Runtime& runtime, const jni::alias_ref<T>& arg) {
+    return JSIConverter<T>::toJSI(runtime, arg);
+  }
+  static inline bool canConvert(jsi::Runtime& runtime, const jsi::Value& value) {
+    return JSIConverter<T>::canConvert(runtime, value);
+  }
+};
+
+// T <> jni::global_ref<T>
+template <typename T>
+struct JSIConverter<jni::global_ref<T>> final {
+  static inline jni::global_ref<T> fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
+    return jni::make_global(JSIConverter<T>::fromJSI(runtime, arg));
   }
   static inline jsi::Value toJSI(jsi::Runtime& runtime, const jni::alias_ref<T>& arg) {
     return JSIConverter<T>::toJSI(runtime, arg);
