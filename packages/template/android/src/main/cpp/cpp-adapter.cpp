d1acda5e0d9e5eb9e485cae05ad2d4e0cd3d893c
Marc Rousavy <me@mrousavy.com>
2024-09-14 15:40:33 +0200

    fix: Fix cpp-adapter in template to properly autolink

--- packages/template/android/src/main/cpp/cpp-adapter.cpp
+++ packages/template/android/src/main/cpp/cpp-adapter.cpp
@@ -1,19 +1,6 @@
 #include <jni.h>
-
-#include "JFunc_void_Person.hpp"
-#include "JFunc_void_std__string.hpp"
-#include "JHybridKotlinTestObjectSpec.hpp"
-
-#include "HybridTestObject.hpp"
-#include <NitroModules/HybridObjectRegistry.hpp>
-#include <fbjni/fbjni.h>
-
-using namespace margelo::nitro::<<androidCxxLibName>>;
+#include "<<androidCxxLibName>>OnLoad.hpp"
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
-  return facebook::jni::initialize(vm, [] {
-    // TODO: Register JNI natives here
-
-    // TODO: Register C++ turbo modules here
-  });
+  return margelo::nitro::<<androidNamespace>>::initialize(vm);
 }
