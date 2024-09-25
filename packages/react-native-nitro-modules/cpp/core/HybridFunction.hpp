377fbd8a797ec941a698d45e68624471af5e605f
Marc Rousavy <me@mrousavy.com>
2024-09-25 11:44:26 +0200

    fix: Provide helpful error message if `this` is not bound (#157)
    * fix: Provide helpful error message if `this` is not bound
    
    * Update HybridFunction.hpp

--- packages/react-native-nitro-modules/cpp/core/HybridFunction.hpp
+++ packages/react-native-nitro-modules/cpp/core/HybridFunction.hpp
@@ -173,7 +173,14 @@ private:
     // 1. Convert jsi::Value to jsi::Object
 #ifdef NITRO_DEBUG
     if (!value.isObject()) [[unlikely]] {
-      throw jsi::JSError(runtime, "Cannot " + getHybridFuncDebugInfo<THybrid>(funcKind, funcName) + " - `this` is not bound!");
+      throw jsi::JSError(runtime, "Cannot " + getHybridFuncDebugInfo<THybrid>(funcKind, funcName) +
+                                      " - `this` is not bound! Suggestions:\n"
+                                      "- Did you accidentally destructure the `HybridObject`? (`const { " +
+                                      funcName +
+                                      " } = ...`)\n"
+                                      "- Did you call `dispose()` on the `HybridObject` before?"
+                                      "- Did you accidentally call `" +
+                                      funcName + "` on the prototype directly?");
     }
 #endif
     jsi::Object object = value.getObject(runtime);
@@ -183,10 +190,12 @@ private:
     if (!object.hasNativeState(runtime)) [[unlikely]] {
       throw jsi::JSError(runtime, "Cannot " + getHybridFuncDebugInfo<THybrid>(funcKind, funcName) +
                                       " - `this` does not have a NativeState! Suggestions:\n"
-                                      "- Did you accidentally destructure the `HybridObject` and lose a reference to the original object?\n"
+                                      "- Did you accidentally destructure the `HybridObject`? (`const { " +
+                                      funcName +
+                                      " } = ...`)\n"
                                       "- Did you call `dispose()` on the `HybridObject` before?"
                                       "- Did you accidentally call `" +
-                                      funcName + "` on the prototype directly?\n");
+                                      funcName + "` on the prototype directly?");
     }
 #endif
 
