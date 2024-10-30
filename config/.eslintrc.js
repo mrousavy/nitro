73ac2d464d831a0594591c518a5797b198a5a5d4
Christoph Pader <chris@margelo.com>
2024-10-30 15:17:09 +0100

    fix: eslint

--- config/.eslintrc.js
+++ config/.eslintrc.js
@@ -1,18 +1,8 @@
 module.exports = {
   root: true,
-  extends: [
-    "@react-native",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "plugin:prettier/recommended",
-    // "prettier",
-  ],
-  // ignores: [
-  //   "node_modules/",
-  //   "lib/"
-  // ],
-  plugins: ["@typescript-eslint", "prettier"],
-  parser: "@typescript-eslint/parser",
+  extends: ['@react-native', 'plugin:prettier/recommended'],
+  ignorePatterns: ['**/node_modules', '**/lib'],
+  plugins: ['prettier'],
   parserOptions: {
     project: true,
     tsconfigRootDir: __dirname,
@@ -20,83 +10,18 @@ module.exports = {
       jsx: true,
     },
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
-        quoteProps: "consistent",
+        quoteProps: 'consistent',
         singleQuote: true,
         tabWidth: 2,
-        trailingComma: "es5",
+        trailingComma: 'es5',
         useTabs: false,
       },
     ],
-    // eslint
-    semi: "off",
-    curly: ["warn", "multi-or-nest", "consistent"],
-    "no-mixed-spaces-and-tabs": ["warn", "smart-tabs"],
-    "no-async-promise-executor": "warn",
-    "require-await": "warn",
-    "no-return-await": "warn",
-    "no-await-in-loop": "warn",
-    "comma-dangle": "off", // prettier already detects this
-    "no-restricted-syntax": [
-      "error",
-      {
-        selector: "TSEnumDeclaration",
-        message:
-          "Enums have various disadvantages, use TypeScript's union types instead.",
-      },
-    ],
-    // prettier
-    "prettier/prettier": ["warn"],
-    // typescript
-    "@typescript-eslint/no-use-before-define": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "error",
-      {
-        vars: "all",
-        args: "after-used",
-        ignoreRestSiblings: false,
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-      },
-    ],
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-unsafe-assignment": "error",
-    "@typescript-eslint/no-floating-promises": "off",
-    // react plugin
-    "react/no-unescaped-entities": "off",
-    // react native plugin
-    "react-native/no-unused-styles": "warn",
-    "react-native/split-platform-components": "off",
-    "react-native/no-inline-styles": "warn",
-    "react-native/no-color-literals": "off",
-    "react-native/no-raw-text": "off",
-    "react-native/no-single-element-style-arrays": "warn",
-    "@typescript-eslint/strict-boolean-expressions": [
-      "error",
-      {
-        allowString: false,
-        allowNullableObject: false,
-        allowNumber: false,
-        allowNullableBoolean: true,
-      },
-    ],
-    "@typescript-eslint/no-non-null-assertion": "error",
-    "@typescript-eslint/no-unnecessary-condition": "error",
-    "@typescript-eslint/consistent-type-imports": "warn",
-
-    // react hooks
-    "react-hooks/exhaustive-deps": [
-      "error",
-      {
-        additionalHooks:
-          "(useDerivedValue|useAnimatedStyle|useAnimatedProps|useWorkletCallback|useFrameProcessor|useSkiaFrameProcessor)",
-      },
-    ],
   },
-};
+}
--- packages/react-native-nitro-modules/.eslintrc.js
+++ packages/react-native-nitro-modules/.eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   root: true,
-  extends: '../config/.eslintrc.js',
+  extends: '../../config/.eslintrc.js',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-}
+};
