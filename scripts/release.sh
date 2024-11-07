3cc4f2f3f9554123597d7183a704720195b6eb7b
Christoph Pader <chris@margelo.com>
2024-11-07 15:29:00 +0100

    chore: Automatic release flow through release script (#302)
    * chore: create release script
    
    * fix: bun.lock
    
    * chore: add echos
    
    * chore: update bundle install scrtip

---
+++ scripts/release.sh
@@ -0,0 +1,16 @@
+echo "Starting the release process..."
+echo "Provided options: $@"
+
+echo "Publishing 'react-native-nitro-modules' to NPM"
+cd packages/react-native-nitro-modules
+bun release $@
+
+echo "Publishing 'nitrogen' to NPM"
+cd ../nitrogen
+bun release $@
+
+echo "Creating a Git bump commit and GitHub release"
+cd ../..
+bun run release-it $@
+
+echo "Successfully released Nitro!"
