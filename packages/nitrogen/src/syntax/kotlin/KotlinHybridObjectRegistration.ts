cd142c0531c9ee34ea28bd126a9aad82eae77a08
Marc Rousavy <me@mrousavy.com>
2024-11-11 17:26:39 +0100

    feat: Move JNI lookup code to `AutolinkedHybridObject` to avoid spaghetti (#319)
    * feat: Move JNI lookup code to `AutolinkedHybridObject`
    
    * fix: Extract name & format
    
    * fix: Remove null check from generated code
    
    * Fix typename
    
    * whoops
    
    * Update AutolinkedHybridObject.hpp
    
    * Update AutolinkedHybridObject.hpp
    
    * hä geht eh so oida
    
    * `AutolinkedHybridObject` -> `ConstructableHybridObject`

--- packages/nitrogen/src/syntax/kotlin/KotlinHybridObjectRegistration.ts
+++ packages/nitrogen/src/syntax/kotlin/KotlinHybridObjectRegistration.ts
@@ -24,10 +24,6 @@ export function createJNIHybridObjectRegistration({
 }: Props): JNIHybridObjectRegistration {
   const { JHybridTSpec } = getHybridObjectName(hybridObjectName)
   const jniNamespace = NitroConfig.getAndroidPackage('c++/jni', jniClassName)
-  const javaNamespace = NitroConfig.getAndroidPackage(
-    'java/kotlin',
-    jniClassName
-  )
 
   return {
     requiredImports: [
@@ -37,42 +33,18 @@ export function createJNIHybridObjectRegistration({
         language: 'c++',
         space: 'system',
       },
+      {
+        name: 'NitroModules/DefaultConstructableObject.hpp',
+        language: 'c++',
+        space: 'system',
+      },
     ],
     cppCode: `
 HybridObjectRegistry::registerHybridObjectConstructor(
   "${hybridObjectName}",
   []() -> std::shared_ptr<HybridObject> {
-    static jni::alias_ref<jni::JClass> javaClass;
-    static jni::JConstructor<${JHybridTSpec}::javaobject()> defaultConstructor;
-    static bool isInitialized = false;
-    try {
-      if (!isInitialized) {
-        javaClass = jni::findClassStatic("${jniNamespace}");
-        defaultConstructor = javaClass->getConstructor<${JHybridTSpec}::javaobject()>();
-        isInitialized = true;
-      }
-    } catch (const jni::JniException& exc) {
-      std::string message = exc.what();
-      if (message.find("ClassNotFoundException")) {
-        throw std::runtime_error("Couldn't find class \`${javaNamespace}\`!\\n"
-                                 "- Make sure the class exists in the specified namespace.\\n"
-                                 "- Make sure the class is not stripped. If you are using ProGuard, add \`@Keep\` and \`@DoNotStrip\` annotations to ${jniClassName}.");
-      } else if (message.find("NoSuchMethodError")) {
-        throw std::runtime_error("Couldn't find ${jniClassName}'s default constructor!\\n"
-                                 "- If you want to autolink ${jniClassName}, add a default constructor that takes zero arguments.\\n"
-                                 "- If you need arguments to create instances of ${jniClassName}, create a separate HybridObject that acts as a factory for this HybridObject to create instances of it with parameters.\\n"
-                                 "- If you already have a default constructor, make sure it is not being stripped. If you are using ProGuard, add \`@Keep\` and \`@DoNotStrip\` annotations to the default constructor.");
-      } else {
-        throw;
-      }
-    }
-
-    auto instance = javaClass->newObject(defaultConstructor);
-#ifdef NITRO_DEBUG
-    if (instance == nullptr) [[unlikely]] {
-      throw std::runtime_error("Failed to create an instance of \\"${JHybridTSpec}\\" - the constructor returned null!");
-    }
-#endif
+    static DefaultConstructableObject<${JHybridTSpec}::javaobject> object("${jniNamespace}");
+    auto instance = object.create();
     auto globalRef = jni::make_global(instance);
     return JNISharedPtr::make_shared_from_jni<${JHybridTSpec}>(globalRef);
   }
