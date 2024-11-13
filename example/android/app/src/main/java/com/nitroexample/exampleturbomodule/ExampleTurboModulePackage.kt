4060660170f83acb2d7c9f817b228637b4014085
Marc Rousavy <me@mrousavy.com>
2024-11-13 13:32:33 +0100

    feat: Implement ExampleTurboModule on Android

---
+++ example/android/app/src/main/java/com/nitroexample/exampleturbomodule/ExampleTurboModulePackage.kt
@@ -0,0 +1,30 @@
+package com.nitroexample.exampleturbomodule
+
+import com.facebook.react.TurboReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.module.model.ReactModuleInfo
+import com.facebook.react.module.model.ReactModuleInfoProvider
+
+class ExampleTurboModulePackage: TurboReactPackage() {
+    override fun getModule(name: String, context: ReactApplicationContext): NativeModule? {
+        if (name == ExampleTurboModuleModule.NAME) {
+            return ExampleTurboModuleModule(context)
+        } else {
+            return null
+        }
+    }
+    override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
+        mapOf(
+            ExampleTurboModuleModule.NAME to ReactModuleInfo(
+                ExampleTurboModuleModule.NAME,
+                ExampleTurboModuleModule.NAME,
+                false, // canOverrideExistingModule
+                false, // needsEagerInit
+                true, // hasConstants
+                false, // isCxxModule
+                true // isTurboModule
+            )
+        )
+    }
+}
