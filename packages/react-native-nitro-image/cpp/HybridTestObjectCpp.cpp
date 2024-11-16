9f2a757cdb4beca7e6b392b2715b3f894d1e450f
Marc Rousavy <me@mrousavy.com>
2024-11-16 23:22:04 +0000

    chore: Format

--- packages/react-native-nitro-image/cpp/HybridTestObjectCpp.cpp
+++ packages/react-native-nitro-image/cpp/HybridTestObjectCpp.cpp
@@ -269,9 +269,7 @@ std::future<double> HybridTestObjectCpp::getValueFromJSCallbackAndWait(const std
 }
 
 std::future<void> HybridTestObjectCpp::promiseThrows() {
-  return std::async(std::launch::async, [=]() {
-    throw std::runtime_error("Promise throws :)");
-  });
+  return std::async(std::launch::async, [=]() { throw std::runtime_error("Promise throws :)"); });
 }
 
 void HybridTestObjectCpp::callAll(const std::function<void()>& first, const std::function<void()>& second,
