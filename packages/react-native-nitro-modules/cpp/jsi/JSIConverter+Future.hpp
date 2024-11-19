ee6561fa5fd12de5e1eb87cb407514da72430c3b
Marc Rousavy <me@mrousavy.com>
2024-11-19 02:22:15 +0100

    chore: Add deprecation notice to `JSIConverter<std::future>`

--- packages/react-native-nitro-modules/cpp/jsi/JSIConverter+Future.hpp
+++ packages/react-native-nitro-modules/cpp/jsi/JSIConverter+Future.hpp
@@ -23,17 +23,21 @@ namespace margelo::nitro {
 using namespace facebook;
 
 // std::future<T> <> Promise<T>
+[[deprecated("Use JSIConverter<std::shared_ptr<Promise<T>>> instead.")]]
 template <typename TResult>
 struct JSIConverter<std::future<TResult>> final {
+  [[deprecated("Use JSIConverter<std::shared_ptr<Promise<T>>> instead.")]]
   static inline std::future<TResult> fromJSI(jsi::Runtime&, const jsi::Value&) {
     throw std::runtime_error("Promise cannot be converted to a native type - it needs to be awaited first!");
   }
 
+  [[deprecated("Use JSIConverter<std::shared_ptr<Promise<T>>> instead.")]]
   static inline jsi::Value toJSI(jsi::Runtime& runtime, std::future<TResult>&& arg) {
     auto promise = Promise<TResult>::awaitFuture(std::move(arg));
     return JSIConverter<std::shared_ptr<Promise<TResult>>>::toJSI(runtime, promise);
   }
 
+  [[deprecated("Use JSIConverter<std::shared_ptr<Promise<T>>> instead.")]]
   static inline bool canConvert(jsi::Runtime&, const jsi::Value&) {
     throw std::runtime_error("jsi::Value of type Promise cannot be converted to std::future yet!");
   }
