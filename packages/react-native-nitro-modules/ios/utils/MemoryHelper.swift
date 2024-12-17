3ff4444c909778660a33bfbb4c67cb28db911ab6
Marc Rousavy <me@mrousavy.com>
2024-12-17 16:11:05 +0100

    Update MemoryHelper.swift

--- packages/react-native-nitro-modules/ios/utils/MemoryHelper.swift
+++ packages/react-native-nitro-modules/ios/utils/MemoryHelper.swift
@@ -9,9 +9,11 @@ import Foundation
 
 public final class MemoryHelper {
   /**
-   * Get the memory size of the given instance.
-   * This only accounts for stack allocated member variables,
-   * not for externally allocated heap allocations like images or buffers.
+   * Get the amount of memory that was allocated using a `malloc`-like allocator
+   * for the given instance, in bytes.
+   * When allocating resources differently (e.g. GPU buffers, or `UIImage`) you
+   * should add their byte sizes to the result of this function to get an object's
+   * total memory footprint.
    */
   public static func getSizeOf(_ instance: AnyObject) -> Int {
     return malloc_size(Unmanaged.passUnretained(instance).toOpaque())
