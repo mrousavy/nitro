3700bbe770e1f688832dbbf119fba73c9c53733b
Marc Rousavy <me@mrousavy.com>
2024-12-19 15:00:48 +0100

    perf: Move even more

--- packages/react-native-nitro-modules/android/src/main/cpp/registry/JHybridObjectRegistry.cpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/registry/JHybridObjectRegistry.cpp
@@ -15,23 +15,26 @@ namespace margelo::nitro {
 void JHybridObjectRegistry::registerHybridObjectConstructor(jni::alias_ref<jni::JClass> clazz, std::string hybridObjectName,
                                                             jni::alias_ref<JHybridObjectInitializer> constructorFn) {
   auto sharedInitializer = jni::make_global(constructorFn);
-  HybridObjectRegistry::registerHybridObjectConstructor(hybridObjectName, [=]() -> std::shared_ptr<HybridObject> {
-    // 1. Call the Java initializer function
-    jni::local_ref<JHybridObject::javaobject> hybridObject = sharedInitializer->call();
+  HybridObjectRegistry::registerHybridObjectConstructor(
+      hybridObjectName,
+      [sharedInitializer = std::move(sharedInitializer),
+       hybridObjectName = std::move(hybridObjectName)]() -> std::shared_ptr<HybridObject> {
+        // 1. Call the Java initializer function
+        jni::local_ref<JHybridObject::javaobject> hybridObject = sharedInitializer->call();
 #ifdef NITRO_DEBUG
-    if (hybridObject == nullptr) [[unlikely]] {
-      throw std::runtime_error("Failed to create HybridObject \"" + hybridObjectName + "\" - the constructor returned null!");
-    }
+        if (hybridObject == nullptr) [[unlikely]] {
+          throw std::runtime_error("Failed to create HybridObject \"" + hybridObjectName + "\" - the constructor returned null!");
+        }
 #endif
 
-    // 2. Make the resulting HybridObject a global (shared) reference
-    jni::global_ref<JHybridObject::javaobject> globalHybridObject = jni::make_global(hybridObject);
-    // 3. Create a shared_ptr from the JNI global reference
-    std::shared_ptr<JHybridObject> sharedCppPart = JNISharedPtr::make_shared_from_jni<JHybridObject>(globalHybridObject);
-    // 4. Up-cast to a HybridObject (kinda unsafe)
-    std::shared_ptr<HybridObject> cast = std::static_pointer_cast<HybridObject>(sharedCppPart);
-    return cast;
-  });
+        // 2. Make the resulting HybridObject a global (shared) reference
+        jni::global_ref<JHybridObject::javaobject> globalHybridObject = jni::make_global(hybridObject);
+        // 3. Create a shared_ptr from the JNI global reference
+        std::shared_ptr<JHybridObject> sharedCppPart = JNISharedPtr::make_shared_from_jni<JHybridObject>(globalHybridObject);
+        // 4. Up-cast to a HybridObject (kinda unsafe)
+        std::shared_ptr<HybridObject> cast = std::static_pointer_cast<HybridObject>(sharedCppPart);
+        return cast;
+      });
 }
 
 void JHybridObjectRegistry::registerNatives() {
