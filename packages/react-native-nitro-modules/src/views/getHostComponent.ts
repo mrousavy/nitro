2410f7052f3673b761f5a95e98d9c130a75d2172
Marc Rousavy <me@mrousavy.com>
2025-01-15 22:53:56 +0100

    feat: Add types to `getHostComponent`

--- packages/react-native-nitro-modules/src/views/getHostComponent.ts
+++ packages/react-native-nitro-modules/src/views/getHostComponent.ts
@@ -2,12 +2,20 @@ import type { HostComponent } from 'react-native'
 // @ts-expect-error this unfortunately isn't typed or default-exported.
 import * as NativeComponentRegistry from 'react-native/Libraries/NativeComponent/NativeComponentRegistry'
 
+interface ViewConfig<Props> {
+  uiViewClassName: string
+  supportsRawText?: boolean
+  bubblingEventTypes: Record<string, unknown>
+  directEventTypes: Record<string, unknown>
+  validAttributes: Record<keyof Props, true>
+}
+
 /**
  * Finds and returns a native view (aka {@linkcode HostComponent}) via the given {@linkcode name}.
  */
 export function getHostComponent<Props>(
   name: string,
-  getViewConfig: () => object
+  getViewConfig: () => ViewConfig<Props>
 ): HostComponent<Props> {
   return NativeComponentRegistry.get<Props>(name, getViewConfig)
 }
