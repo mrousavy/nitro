ccfc707f3a0799f16a51eec3f89d5419351f580f
Marc Rousavy <me@mrousavy.com>
2025-01-16 01:27:26 +0100

    format

--- packages/react-native-nitro-modules/cpp/views/CachedProp.hpp
+++ packages/react-native-nitro-modules/cpp/views/CachedProp.hpp
@@ -11,32 +11,33 @@
 
 namespace margelo::nitro {
 
-  using namespace facebook;
+using namespace facebook;
 
-  template <typename T>
-  struct CachedProp {
-  public:
-    T value;
-    OwningReference<jsi::Value> jsiValue;
-    bool isDirty;
-    
-  public:
-    bool equals(jsi::Runtime& runtime, const jsi::Value& other) const {
-      if (!jsiValue) return false;
-      return jsi::Value::strictEquals(runtime, *jsiValue, other);
+template <typename T>
+struct CachedProp {
+public:
+  T value;
+  OwningReference<jsi::Value> jsiValue;
+  bool isDirty;
+
+public:
+  bool equals(jsi::Runtime& runtime, const jsi::Value& other) const {
+    if (!jsiValue)
+      return false;
+    return jsi::Value::strictEquals(runtime, *jsiValue, other);
+  }
+
+public:
+  static CachedProp<T> fromRawValue(jsi::Runtime& runtime, const jsi::Value& value, const CachedProp<T>& oldProp) {
+    if (oldProp.equals(runtime, value)) {
+      // jsi::Value hasn't changed - no need to convert it again!
+      return oldProp;
     }
-    
-  public:
-    static CachedProp<T> fromRawValue(jsi::Runtime& runtime, const jsi::Value& value, const CachedProp<T>& oldProp) {
-      if (oldProp.equals(runtime, value)) {
-        // jsi::Value hasn't changed - no need to convert it again!
-        return oldProp;
-      }
-      auto converted = JSIConverter<T>::fromJSI(runtime, value);
-      auto cache = JSICache::getOrCreateCache(runtime);
-      auto cached = cache.makeShared(jsi::Value(runtime, value));
-      return CachedProp<T>(std::move(converted), std::move(cached), /* isDirty */ true);
-    }
-  };
+    auto converted = JSIConverter<T>::fromJSI(runtime, value);
+    auto cache = JSICache::getOrCreateCache(runtime);
+    auto cached = cache.makeShared(jsi::Value(runtime, value));
+    return CachedProp<T>(std::move(converted), std::move(cached), /* isDirty */ true);
+  }
+};
 
 } // namespace margelo::nitro
