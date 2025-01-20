140d8163b4d5047a9cb5cdb5b7bc734a7fb7b30f
Marc Rousavy <marcrousavy@hotmail.com>
2025-01-20 16:47:53 +0100

    perf: Use `static_cast<>` for props

--- packages/react-native-nitro-image/nitrogen/generated/android/c++/views/JHybridTestViewStateUpdater.cpp
+++ packages/react-native-nitro-image/nitrogen/generated/android/c++/views/JHybridTestViewStateUpdater.cpp
@@ -20,8 +20,7 @@ void JHybridTestViewStateUpdater::updateViewProps(jni::alias_ref<jni::JClass>,
                                            jni::alias_ref<react::StateWrapperImpl::javaobject> stateWrapper) {
   JHybridTestViewSpec* view = javaView->cthis();
   const react::State& state = stateWrapper->cthis()->getState();
-  // TODO: Can this be a static_cast?
-  const auto& concreteState = dynamic_cast<const ConcreteStateData&>(state);
+  const auto& concreteState = static_cast<const ConcreteStateData&>(state);
   const HybridTestViewState& data = concreteState.getData();
   const std::optional<HybridTestViewProps>& maybeProps = data.getProps();
   if (!maybeProps.has_value()) {
