fdd6669ae25996f986eb65f4e3ee555474277d3d
Marc Rousavy <me@mrousavy.com>
2025-01-23 15:45:28 +0100

    background color in android?

--- packages/react-native-nitro-image/android/src/main/java/com/margelo/nitro/image/HybridTestView.kt
+++ packages/react-native-nitro-image/android/src/main/java/com/margelo/nitro/image/HybridTestView.kt
@@ -1,5 +1,6 @@
 package com.margelo.nitro.image
 
+import android.graphics.Color
 import android.view.View
 import androidx.annotation.Keep
 import com.facebook.proguard.annotations.DoNotStrip
@@ -8,12 +9,23 @@ import com.margelo.nitro.NitroModules
 @Keep
 @DoNotStrip
 class HybridTestView: HybridTestViewSpec() {
-    override var someProp: Boolean = false
+    // View
+    override val view: View = View(NitroModules.applicationContext)
+
+    // Props
+    private var _someProp = false
+    override var someProp: Boolean
+        get() = _someProp
+        set(value) {
+            _someProp = value
+            val color = if (value) Color.RED else Color.BLUE
+            view.setBackgroundColor(color)
+        }
+
     override var someCallback: (someParam: Double) -> Unit = {}
 
     override fun someFunc(someParam: Double): Boolean {
         return someProp
     }
 
-    override val view: View = View(NitroModules.applicationContext)
 }
