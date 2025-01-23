1b97dc7b3baf2531a8f977bba463b536ceb34603
Marc Rousavy <me@mrousavy.com>
2025-01-23 13:33:48 +0100

    chore: Upgrade to react-native 0.77.0 (#511)
    * chore: Upgrade to react-native 0.77.0
    
    * feat: Add `RCTAppDependencyProvider`
    
    * feat: Upgrade pods and move swift file
    
    * fix: Update libs that broke
    
    * Update Podfile.lock

--- example/ios/NitroExample/AppDelegate.swift
+++ example/ios/NitroExample/AppDelegate.swift
@@ -8,12 +8,13 @@
 import UIKit
 import React
 import React_RCTAppDelegate
+import ReactAppDependencyProvider
  
 @main
 class AppDelegate: RCTAppDelegate {
   override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     self.moduleName = "NitroExample"
-    //self.dependencyProvider = RCTAppDependencyProvider()
+    self.dependencyProvider = RCTAppDependencyProvider()
  
     // You can add your custom initial props in the dictionary below.
     // They will be passed down to the ViewController used by React Native.
