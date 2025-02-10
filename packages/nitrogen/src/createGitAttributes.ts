4907639a448b2b218f6e7b93a0e1319b16bd56b8
Marc Rousavy <me@mrousavy.com>
2025-02-10 22:22:16 +0100

    fix: Fix `f.path` sometimes being undefined (#533)
    * fix: Append `.gitattributes` file as well
    
    * fix: Fix `f.path` sometimes being undefined
    
    * Update createGitAttributes.ts

--- packages/nitrogen/src/createGitAttributes.ts
+++ packages/nitrogen/src/createGitAttributes.ts
@@ -1,9 +1,13 @@
 import fs from 'fs/promises'
 import path from 'path'
 
+const GIT_ATTRIBUTES_CONTENT = `
+* linguist-generated
+`
+
 export async function createGitAttributes(folder: string): Promise<string> {
   const file = path.join(folder, '.gitattributes')
   // Marks all files in this current folder as "generated"
-  await fs.writeFile(file, `* linguist-generated`)
+  await fs.writeFile(file, GIT_ATTRIBUTES_CONTENT.trim() + '\n', 'utf-8')
   return file
 }
