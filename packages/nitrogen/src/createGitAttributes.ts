12d16f167a7722224540c0f78962fb0bbd28d763
Marc Rousavy <me@mrousavy.com>
2025-02-10 13:10:32 -0800

    fix: Append `.gitattributes` file as well

--- packages/nitrogen/src/createGitAttributes.ts
+++ packages/nitrogen/src/createGitAttributes.ts
@@ -1,8 +1,9 @@
 import fs from 'fs/promises'
 import path from 'path'
 
-export async function createGitAttributes(folder: string): Promise<void> {
+export async function createGitAttributes(folder: string): Promise<string> {
   const file = path.join(folder, '.gitattributes')
   // Marks all files in this current folder as "generated"
   await fs.writeFile(file, `* linguist-generated`)
+  return file
 }
