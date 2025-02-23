e038cd5766a58ce29d326764e12ebec2b42cb32c
Marc Rousavy <me@mrousavy.com>
2025-02-23 14:34:45 +0100

    Docs

--- packages/react-native-nitro-modules/cpp/utils/JSCallback.hpp
+++ packages/react-native-nitro-modules/cpp/utils/JSCallback.hpp
@@ -14,7 +14,9 @@ struct JSIConverter;
 
 #include "BorrowingReference.hpp"
 #include "JSIConverter.hpp"
+#include "NitroDefines.hpp"
 #include "Promise.hpp"
+#include "TypeInfo.hpp"
 #include <functional>
 #include <jsi/jsi.h>
 
@@ -31,7 +33,21 @@ public:
   SyncJSCallback(jsi::Runtime& runtime, BorrowingReference<jsi::Function>&& function) : _runtime(runtime), _function(std::move(function)) {}
 
 public:
+  /**
+   * Calls this `SyncJSCallback` synchronously, and
+   * returns it's result (`R`).
+   * The callee is responsible for ensuring that the
+   * underlying `jsi::Function` can actually be called from this Thread.
+   * In Debug, sanity checks are made to ensure the `jsi::Function` is still alive.
+   */
   R call(Args... args) const {
+#ifdef NITRO_DEBUG
+    if (!_function) [[unlikely]] {
+      throw std::runtime_error("Cannot call " + TypeInfo::getFriendlyTypename<SyncJSCallback<R(Args...)>>() +
+                               " - the underlying `jsi::Function` has already been deleted!");
+    }
+#endif
+
     jsi::Value result = _function->call(_runtime, JSIConverter<std::decay_t<Args>>::toJSI(_runtime, args)...);
     if constexpr (std::is_void_v<R>) {
       // It's returning void. No result
@@ -64,9 +80,21 @@ public:
       : _callback(std::move(callback)), _dispatcher(dispatcher) {}
 
 public:
+  /**
+   * Calls this `AsyncJSCallback` asynchronously, and returns a Promise that
+   * can be awaited to receive the returned result (`R`) from JS.
+   * This can be called from any Thread.
+   * If the Runtime is no longer alive, this method throws.
+   */
   std::shared_ptr<Promise<R>> call(Args... args) const {
     return _dispatcher->runAsyncAwaitable<R>([callback = _callback, ... args = std::move(args)]() { return callback.call(args...); });
   }
+  /**
+   * Calls this `AsyncJSCallback` asynchronously, and ignore
+   * any results or completions.
+   * This can be called from any Thread.
+   * If the Runtime is no longer alive, this method ignores the function call.
+   */
   void callAndForget(Args... args) const {
     _dispatcher->runAsync([callback = _callback, ... args = std::move(args)]() { return callback.call(args...); });
   }
