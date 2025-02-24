44e20e4fe8cf776eb88b151480e1b9916617e79c
Marc Rousavy <me@mrousavy.com>
2025-02-24 14:56:41 +0100

    chore: Remove deprecated Java `HybridObjectRegistry`

--- packages/react-native-nitro-modules/android/src/main/cpp/JNIOnLoad.cpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/JNIOnLoad.cpp
@@ -3,7 +3,6 @@
 #include "JAnyMap.hpp"
 #include "JAnyValue.hpp"
 #include "JArrayBuffer.hpp"
-#include "JHybridObjectRegistry.hpp"
 #include "JNitroModules.hpp"
 #include "JPromise.hpp"
 #include <fbjni/fbjni.h>
@@ -14,7 +13,6 @@ using namespace margelo::nitro;
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, [] {
     // 1. Initialize all core Nitro Java bindings
-    JHybridObjectRegistry::registerNatives();
     JArrayBuffer::registerNatives();
     JAnyMap::registerNatives();
     JAnyValue::registerNatives();
