c2cda69dc4f295a9635631048751a710a1613863
Marc Rousavy <me@mrousavy.com>
2025-07-01 17:46:21 +0200

    chore: No need for `public`

--- packages/react-native-nitro-modules/ios/utils/Date+fromChrono.swift
+++ packages/react-native-nitro-modules/ios/utils/Date+fromChrono.swift
@@ -11,7 +11,7 @@ public extension Date {
   /**
    * Create a new `Date` object from the given `std::chrono::system_clock::time_point` value.
    */
-  public init(fromChrono date: margelo.nitro.chrono_time) {
+  init(fromChrono date: margelo.nitro.chrono_time) {
     let millisecondsSinceEpoch = margelo.nitro.millisecondsSinceEpochFromChronoDate(date)
     self = .init(timeIntervalSince1970: millisecondsSinceEpoch / 1_000)
   }
@@ -19,7 +19,7 @@ public extension Date {
   /**
    * Converts this `Date` object to a `std::chrono::system_clock::time_point` value.
    */
-  public func toCpp() -> margelo.nitro.chrono_time {
+  func toCpp() -> margelo.nitro.chrono_time {
     let secondsSinceEpoch = self.timeIntervalSince1970
     let millisecondsSinceEpoch = secondsSinceEpoch * 1_000
     return margelo.nitro.chronoDateFromMillisecondsSinceEpoch(millisecondsSinceEpoch)
