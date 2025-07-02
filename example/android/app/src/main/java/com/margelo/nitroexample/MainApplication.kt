b92275e65ff329cdbe163ca49c29437c6e1a41f6
Marc Rousavy <me@mrousavy.com>
2025-07-02 10:11:46 +0200

    fix: Move android example to `com.margelo....` namespace

--- example/android/app/src/main/java/com/margelo/nitroexample/MainApplication.kt
+++ example/android/app/src/main/java/com/margelo/nitroexample/MainApplication.kt
@@ -9,7 +9,7 @@ import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.nitroexample.exampleturbomodule.ExampleTurboModulePackage
+import com.margelo.nitroexample.exampleturbomodule.ExampleTurboModulePackage
 
 class MainApplication : Application(), ReactApplication {
 
