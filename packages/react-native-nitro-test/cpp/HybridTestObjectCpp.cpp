e8a917f2fa3efeae0c13396288592b2bd09e6898
Marc Rousavy <me@mrousavy.com>
2025-07-16 16:33:04 +0200

    Update HybridTestObjectCpp.cpp

--- packages/react-native-nitro-test/cpp/HybridTestObjectCpp.cpp
+++ packages/react-native-nitro-test/cpp/HybridTestObjectCpp.cpp
@@ -400,12 +400,11 @@ HybridTestObjectCpp::getValueFromJsCallback(const std::function<std::shared_ptr<
   });
 }
 
-
 void HybridTestObjectCpp::callbackBothStructs(const std::function<void(const Person& /* person */)>& first,
                                               const std::function<void(const Car& /* car */)>& second) {
   Person person("Marc", 25);
   first(person);
-  
+
   Car car = getCar();
   second(car);
 }
