39b8915f9af4fd0d93bd437cb80c7463533e48af
Marc Rousavy <me@mrousavy.com>
2025-08-19 12:14:46 +0200

    feat: Add `NitroModules.isHybridObject(...)` (#828)
    * feat: Add `NitroModules.isHybridObject(...)`
    
    * chore: Move down

--- packages/react-native-nitro-modules/cpp/entrypoint/HybridNitroModulesProxy.cpp
+++ packages/react-native-nitro-modules/cpp/entrypoint/HybridNitroModulesProxy.cpp
@@ -18,6 +18,7 @@ void HybridNitroModulesProxy::loadHybridMethods() {
   registerHybrids(this, [](Prototype& prototype) {
     prototype.registerHybridMethod("createHybridObject", &HybridNitroModulesProxy::createHybridObject);
     prototype.registerHybridMethod("hasHybridObject", &HybridNitroModulesProxy::hasHybridObject);
+    prototype.registerRawHybridMethod("isHybridObject", 1, &HybridNitroModulesProxy::isHybridObject);
     prototype.registerHybridMethod("getAllHybridObjectNames", &HybridNitroModulesProxy::getAllHybridObjectNames);
 
     prototype.registerHybridMethod("box", &HybridNitroModulesProxy::box);
@@ -53,6 +54,20 @@ jsi::Value HybridNitroModulesProxy::hasNativeState(jsi::Runtime& runtime, const 
     return false;
   }
   return args[0].getObject(runtime).hasNativeState(runtime);
+}
+
+jsi::Value HybridNitroModulesProxy::isHybridObject(jsi::Runtime& runtime, const jsi::Value&, const jsi::Value* args, size_t size) {
+  if (size != 1 || !args[0].isObject()) {
+    return false;
+  }
+  jsi::Object object = args[0].getObject(runtime);
+  if (!object.hasNativeState(runtime)) {
+    return false;
+  }
+  std::shared_ptr<jsi::NativeState> nativeState = object.getNativeState(runtime);
+  std::shared_ptr<HybridObject> maybeHybridObject = std::dynamic_pointer_cast<HybridObject>(nativeState);
+  bool isHybrid = maybeHybridObject != nullptr;
+  return jsi::Value(isHybrid);
 }
 
 std::shared_ptr<HybridObject> HybridNitroModulesProxy::updateMemorySize(const std::shared_ptr<HybridObject>& hybridObject) {
