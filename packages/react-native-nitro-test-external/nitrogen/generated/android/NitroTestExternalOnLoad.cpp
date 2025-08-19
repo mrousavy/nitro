32d52ea60d4d6eb322656c9b66774fc58fc77436
Marc Rousavy <me@mrousavy.com>
2025-08-19 16:05:23 +0200

    Update NitroTestExternalOnLoad.cpp

--- packages/react-native-nitro-test-external/nitrogen/generated/android/NitroTestExternalOnLoad.cpp
+++ packages/react-native-nitro-test-external/nitrogen/generated/android/NitroTestExternalOnLoad.cpp
@@ -35,8 +35,7 @@ int initialize(JavaVM* vm) {
       []() -> std::shared_ptr<HybridObject> {
         static DefaultConstructableObject<JHybridSomeExternalObjectSpec::javaobject> object("com/margelo/nitro/test/external/HybridSomeExternalObject");
         auto instance = object.create();
-        auto globalRef = jni::make_global(instance);
-        return globalRef->cthis()->shared();
+        return instance->cthis()->shared();
       }
     );
   });
