6a75fbd2f0ee80fdc686206b0cfb6bc6b60eaf28
Marc Rousavy <me@mrousavy.com>
2025-08-28 13:19:38 +0200

    Extract it

--- packages/nitrogen/src/syntax/createType.ts
+++ packages/nitrogen/src/syntax/createType.ts
@@ -42,6 +42,7 @@ import {
   isPromise,
   isRecord,
 } from './isCoreType.js'
+import { getCustomTypeConfig } from './getCustomTypeConfig.js'
 
 function getHybridObjectName(type: TSMorphType): string {
   const symbol = isHybridView(type) ? type.getAliasSymbol() : type.getSymbol()
@@ -253,65 +254,8 @@ export function createType(
       return new ErrorType()
     } else if (isCustomType(type)) {
       // Custom C++ type (manually written)
-      const parts = type.getIntersectionTypes()
-      for (const part of parts) {
-        const typeNameProperty = part.getProperty('__customTypeName')
-        if (typeNameProperty == null) continue
-        const typeConfigProperty = part.getProperty('__customTypeConfig')
-        if (typeConfigProperty == null) continue
-
-        const declaration = type.getAliasSymbolOrThrow().getDeclarations()[0]
-        if (declaration == null)
-          throw new Error(`Type has no declaration! ${type.getText()}`)
-
-        const typeNameTypeOrUndefined =
-          typeNameProperty.getTypeAtLocation(declaration)
-        const typeConfigTypeOrUndefined =
-          typeConfigProperty.getTypeAtLocation(declaration)
-
-        const typeNameType = typeNameTypeOrUndefined
-          .getUnionTypes()
-          .find((t) => t.isLiteral())
-        if (typeNameType == null) continue
-        const typeConfigType = typeConfigTypeOrUndefined
-          .getUnionTypes()
-          .find((t) => t.isObject())
-        if (typeConfigType == null) continue
-
-        const typeName = typeNameType.getLiteralValue()
-        if (typeof typeName !== 'string') {
-          throw new Error(
-            `CustomType's second argument (TypeName) needs to be a string! Instead, it is a ${typeof typeName} (${typeName})`
-          )
-        }
-        const includeType = typeConfigType
-          .getPropertyOrThrow('include')
-          .getTypeAtLocation(declaration)
-        const include = includeType.getLiteralValue()
-        if (typeof include !== 'string')
-          throw new Error(
-            `CustomType's third argument (Config) needs to contain { include }, which should be a string! (It is ${includeType.getText()} instead)`
-          )
-        const canBePassedByReferenceType = typeConfigType
-          .getProperty('canBePassedByReference')
-          ?.getTypeAtLocation(declaration)
-          .getUnionTypes()
-          .find((u) => u.isBooleanLiteral())
-          ?.getLiteralValue()
-        const canBePassedByReference =
-          typeof canBePassedByReferenceType === 'boolean'
-            ? canBePassedByReferenceType
-            : false
-
-        return new CustomType(typeName, {
-          include: include,
-          canBePassedByReference: canBePassedByReference,
-        })
-      }
-
-      throw new Error(
-        `Type looks like a CustomType<...>, but doesn't have generic arguments! ${type.getText()}`
-      )
+      const { name, config } = getCustomTypeConfig(type)
+      return new CustomType(name, config)
     } else if (type.isEnum()) {
       // It is an enum. We need to generate a C++ declaration for the enum
       const typename = type.getSymbolOrThrow().getEscapedName()
