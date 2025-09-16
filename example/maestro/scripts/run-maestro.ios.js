18fe92fcba0c6fcdcf04b7c0f5eaa621a657427a
riteshshukla04 <riteshshukla2381@gmail.com>
2025-09-16 18:31:18 +0530

    change-package-name

--- example/maestro/scripts/run-maestro.ios.js
+++ example/maestro/scripts/run-maestro.ios.js
@@ -14,7 +14,7 @@ function sleep(ms) {
 async function main() {
     let metroProcess = null
 	console.log('📱 Starting simulator...')
-	execSync(`xcrun simctl boot "iPhone 16"`)
+	execSync(`xcrun simctl boot "iPhone 15"`)
 
 	console.log('📱 Installing app...')
 	
