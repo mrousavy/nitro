e1be4a7b3876860385c89256152cf6244f30c433
Marc Rousavy <me@mrousavy.com>
2025-09-27 13:54:58 +0200

    Drop Shadow

--- docs/src/plugins/generate-og-images/NitroOgCard.tsx
+++ docs/src/plugins/generate-og-images/NitroOgCard.tsx
@@ -14,6 +14,31 @@ const BLUE_1 = 'rgb(131, 199, 235)'
 const BLUE_2 = 'rgb(81, 159, 199)'
 const BLUE_3 = 'rgb(30, 106, 140)'
 
+function generateTextShadow(distanceH: number, distanceV: number): string {
+  const STEP = 0.5
+  let counterH = 0
+  let counterV = 0
+
+  const shadows: string[] = []
+  const stepCount = Math.max(Math.abs(distanceH), Math.abs(distanceV)) / STEP
+  for (let i = 0; i < stepCount; i++) {
+    shadows.push(`${counterH}px ${counterV}px 0px ${BLACK}`)
+    counterH += distanceH / stepCount
+    counterV += distanceV / stepCount
+  }
+  return shadows.join(', ')
+}
+
+const textShadow = generateTextShadow(-20, 15)
+
+const steps = 28;                 // extrusion length
+const color = '#223048';          // shadow color
+const dx = -1, dy = 1;            // direction: bottom-left
+const extrude = Array.from({length: steps},
+  (_, i) => `drop-shadow(${dx*(i+1)}px ${dy*(i+1)}px 0 ${color})`
+).join(' ');
+console.log(extrude)
+
 export function NitroOgCard({ title }: Props): React.ReactElement {
   return (
     <div
@@ -62,7 +87,9 @@ export function NitroOgCard({ title }: Props): React.ReactElement {
               color: "white",
               maxWidth: '100%',
               WebkitTextStrokeColor: BLACK,
-              WebkitTextStrokeWidth: 50
+              WebkitTextStrokeWidth: 14,
+              paintOrder: 'stroke fill',
+              filter: `${extrude} drop-shadow(0 6px 8px rgba(0,0,0,.25))`, // soft ambient
             }}
           >
             NitroModules
