e4291710662b4d26cab15b4065c0dda2ba61e35b
Marc Rousavy <me@mrousavy.com>
2025-09-30 18:17:46 +0200

    chore: Remove unused import

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,5 +1,5 @@
 import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config, HtmlTagObject } from '@docusaurus/types';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
