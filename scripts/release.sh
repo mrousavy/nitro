e048f4980a62dee5681579fcff02ad50b7e717a3
Marc Rousavy <me@mrousavy.com>
2025-10-15 12:43:04 +0200

    Add `NITRO_ENABLE_LOGS` flag that will be false on npm

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,6 +5,9 @@ set -e
 echo "Starting the release process..."
 echo "Provided options: $@"
 
+echo "Flipping NITRO_ENABLE_LOGS to false..."
+sed -i "s/#define NITRO_ENABLE_LOGS 1/#define NITRO_ENABLE_LOGS 0/g" packages/react-native-nitro-modules/cpp/NitroLogger.hpp
+
 echo "Publishing 'react-native-nitro-modules' to NPM"
 cd packages/react-native-nitro-modules
 bun release $@
