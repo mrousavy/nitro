f06dee8500b84c89b2d2bd9a09cc7624f4ee9dd6
Marc Rousavy <me@mrousavy.com>
2025-10-16 12:03:22 +0200

    chore: Split `JSIConverter<HybridObject>` and `JSIConverter<NativeState>`

--- packages/react-native-nitro-modules/cpp/jsi/JSIConverter+HybridObject.hpp
+++ packages/react-native-nitro-modules/cpp/jsi/JSIConverter+HybridObject.hpp
@@ -7,6 +7,9 @@
 // Forward declare a few of the common types that might have cyclic includes.
 namespace margelo::nitro {
 class HybridObject;
+
+template <typename T, typename Enable>
+struct JSIConverter;
 } // namespace margelo::nitro
 
 #include "IsSharedPtrTo.hpp"
@@ -21,66 +24,27 @@ using namespace facebook;
 
 // HybridObject(NativeState) <> {}
 template <typename T>
-struct JSIConverter<T, std::enable_if_t<is_shared_ptr_to_v<T, jsi::NativeState>>> final {
+struct JSIConverter<T, std::enable_if_t<is_shared_ptr_to_v<T, HybridObject>>> final {
   using TPointee = typename T::element_type;
 
   static inline T fromJSI(jsi::Runtime& runtime, const jsi::Value& arg) {
-#ifdef NITRO_DEBUG
-    if (!arg.isObject()) [[unlikely]] {
-      if (arg.isUndefined()) [[unlikely]] {
-        throw jsi::JSError(runtime, invalidTypeErrorMessage("undefined", "It is undefined!"));
-      } else {
-        std::string stringRepresentation = arg.toString(runtime).utf8(runtime);
-        throw jsi::JSError(runtime, invalidTypeErrorMessage(stringRepresentation, "It is not an object!"));
-      }
-    }
-#endif
-    jsi::Object object = arg.asObject(runtime);
-
-#ifdef NITRO_DEBUG
-    if (!object.hasNativeState<TPointee>(runtime)) [[unlikely]] {
-      if (!object.hasNativeState(runtime)) [[unlikely]] {
-        std::string stringRepresentation = arg.toString(runtime).utf8(runtime);
-        throw jsi::JSError(runtime, invalidTypeErrorMessage(stringRepresentation, "It does not have a NativeState!"));
-      } else {
-        std::string stringRepresentation = arg.toString(runtime).utf8(runtime);
-        throw jsi::JSError(runtime, invalidTypeErrorMessage(stringRepresentation, "It has a different NativeState<T>!"));
-      }
-    }
-#endif
-    std::shared_ptr<jsi::NativeState> nativeState = object.getNativeState(runtime);
-    return std::dynamic_pointer_cast<TPointee>(nativeState);
+    // forward to base
+    return JSIConverter<std::shared_ptr<jsi::NativeState>>::fromJSI(runtime, arg);
   }
 
   static inline jsi::Value toJSI(jsi::Runtime& runtime, const T& arg) {
     if (arg == nullptr) [[unlikely]] {
       std::string typeName = TypeInfo::getFriendlyTypename<TPointee>();
-      throw jsi::JSError(runtime, "Cannot convert `nullptr` to NativeState<" + typeName + ">!");
+      throw jsi::JSError(runtime, "Cannot convert `nullptr` to HybridObject<" + typeName + ">!");
     }
 
-    if constexpr (std::is_base_of_v<HybridObject, TPointee>) {
-      // It's a HybridObject - use it's internal constructor which caches jsi::Objects for proper memory management!
-      return arg->toObject(runtime);
-    } else {
-      // It's any other kind of jsi::NativeState - just create it as normal. This will not have a prototype then!
-      jsi::Object object(runtime);
-      object.setNativeState(runtime, arg);
-      return object;
-    }
+    // The internal ->toObject method caches jsi::Objects
+    return arg->toObject(runtime);
   }
 
   static inline bool canConvert(jsi::Runtime& runtime, const jsi::Value& value) {
-    if (value.isObject()) {
-      jsi::Object object = value.getObject(runtime);
-      return object.hasNativeState<TPointee>(runtime);
-    }
-    return false;
-  }
-
-private:
-  static std::string invalidTypeErrorMessage(const std::string& typeDescription, const std::string& reason) {
-    std::string typeName = TypeInfo::getFriendlyTypename<TPointee>();
-    return "Cannot convert \"" + typeDescription + "\" to NativeState<" + typeName + ">! " + reason;
+    // forward to base
+    return JSIConverter<std::shared_ptr<jsi::NativeState>>::canConvert(runtime, value);
   }
 };
 
