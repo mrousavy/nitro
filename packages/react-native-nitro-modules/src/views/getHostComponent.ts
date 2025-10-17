39f3d2b7edcefa00d55777913542fc5d07c72ede
Marc Rousavy <me@mrousavy.com>
2025-10-17 19:14:21 +0200

    feat: Try importing `NativeComponentRegistry` the new way

--- packages/react-native-nitro-modules/src/views/getHostComponent.ts
+++ packages/react-native-nitro-modules/src/views/getHostComponent.ts
@@ -1,12 +1,10 @@
 import { Platform, type HostComponent, type ViewProps } from 'react-native'
-// @ts-expect-error this unfortunately isn't typed or default-exported.
-// eslint-disable-next-line @react-native/no-deep-imports
-import * as NativeComponentRegistry from 'react-native/Libraries/NativeComponent/NativeComponentRegistry'
 import type {
   HybridView,
   HybridViewMethods,
   HybridViewProps,
 } from './HybridView'
+import { NativeComponentRegistry } from './NativeComponentRegistry'
 
 type AttributeValue<T, V = T> =
   | boolean
