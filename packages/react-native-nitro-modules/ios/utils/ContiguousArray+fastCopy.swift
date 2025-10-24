487cff425e61adff1110e592d4f943daf9a758d3
Marc Rousavy <me@mrousavy.com>
2025-10-24 11:43:43 +0200

    Docs

--- packages/react-native-nitro-modules/ios/utils/ContiguousArray+fastCopy.swift
+++ packages/react-native-nitro-modules/ios/utils/ContiguousArray+fastCopy.swift
@@ -5,15 +5,26 @@
 //  Created by Marc Rousavy on 24.10.2025.
 //
 
-import Foundation
 import Cxx
+import Foundation
 
 extension ContiguousArray {
-  public static func fastCopy(vector: some CxxVector<Element> & Sequence<Element>) -> ContiguousArray {
+  ///
+  /// Copies the given `vector` into a new `ContiguosArray`.
+  ///
+  /// @discussion
+  /// If the given `vector` provides access to it's contiguous storage,
+  /// this method performs a fast-copy (= direct `memcpy`).
+  /// Otherwise, a normal copy (= looping over each element) is made.
+  ///
+  public static func fastCopy(vector: some CxxVector<Element> & Sequence<Element>)
+    -> ContiguousArray
+  {
     let size = Int(vector.size())
-    
+
     let result = vector.withContiguousStorageIfAvailable { data in
-      return ContiguousArray<Element>(unsafeUninitializedCapacity: size) { buffer, initializedCount in
+      return ContiguousArray<Element>(unsafeUninitializedCapacity: size) {
+        buffer, initializedCount in
         let bytesCount = size * MemoryLayout<Element>.stride
         memcpy(buffer.baseAddress!, data.baseAddress, bytesCount)
         initializedCount = size
