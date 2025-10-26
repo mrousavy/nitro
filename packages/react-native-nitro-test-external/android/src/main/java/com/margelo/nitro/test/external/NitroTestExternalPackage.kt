da25da24cfa5d2a5a0139828a861697e42782806
HyunWoo Lee <l2hyunwoo@gmail.com>
2025-10-27 01:32:36 +0900

    chore: use BaseReactPackage instead of deprecated TurboReactPackage

--- packages/react-native-nitro-test-external/android/src/main/java/com/margelo/nitro/test/external/NitroTestExternalPackage.kt
+++ packages/react-native-nitro-test-external/android/src/main/java/com/margelo/nitro/test/external/NitroTestExternalPackage.kt
@@ -1,11 +1,11 @@
 package com.margelo.nitro.test.external
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
 
-public class NitroTestExternalPackage : TurboReactPackage() {
+public class NitroTestExternalPackage : BaseReactPackage() {
   override fun getModule(
     name: String,
     reactContext: ReactApplicationContext,
--- packages/react-native-nitro-test/android/src/main/java/com/margelo/nitro/test/NitroTestPackage.kt
+++ packages/react-native-nitro-test/android/src/main/java/com/margelo/nitro/test/NitroTestPackage.kt
@@ -1,13 +1,13 @@
 package com.margelo.nitro.test
 
-import com.facebook.react.TurboReactPackage
+import com.facebook.react.BaseReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.uimanager.ViewManager
 import com.margelo.nitro.test.views.HybridTestViewManager
 
-class NitroTestPackage : TurboReactPackage() {
+class NitroTestPackage : BaseReactPackage() {
   override fun getModule(
     name: String,
     reactContext: ReactApplicationContext,
