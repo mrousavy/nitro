a24a6c065d3f9bb2ed50505a6ab9569c5d9b2a4a
Marc Rousavy <me@mrousavy.com>
2025-10-27 22:22:20 +0100

    fix: Exports

--- packages/react-native-nitro-test/src/index.ts
+++ packages/react-native-nitro-test/src/index.ts
@@ -1,32 +1,33 @@
 import { NitroModules } from 'react-native-nitro-modules'
 import {
-  type Base,
-  type Child,
   type TestObjectCpp,
   type TestObjectSwiftKotlin,
 } from './specs/TestObject.nitro'
 import { type PlatformObject } from './specs/PlatformObject.nitro'
+import type { Base } from './specs/Base.nitro'
+import type { Child } from './specs/Child.nitro'
+import { TestView, type TestViewRef } from './views/TestView'
 
-export * from './specs/TestObject.nitro'
+// Export all Hybrid Object types
+export type {
+  TestObjectCpp,
+  TestObjectSwiftKotlin,
+  Child,
+  Base,
+  PlatformObject,
+}
 
-export * from './views/TestView'
-
-/**
- * The Hybrid Test Object in C++
- */
+// Export all HybridObject singleton instances
 export const HybridTestObjectCpp =
   NitroModules.createHybridObject<TestObjectCpp>('TestObjectCpp')
-
-/**
- * The Hybrid Test Object in Swift/Kotlin
- */
 export const HybridTestObjectSwiftKotlin =
   NitroModules.createHybridObject<TestObjectSwiftKotlin>(
     'TestObjectSwiftKotlin'
   )
-
 export const HybridBase = NitroModules.createHybridObject<Base>('Base')
 export const HybridChild = NitroModules.createHybridObject<Child>('Child')
-
 export const HybridPlatformObject =
   NitroModules.createHybridObject<PlatformObject>('PlatformObject')
+
+// Export View (+ it's ref type)
+export { TestView, type TestViewRef }
