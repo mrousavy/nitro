075e5c14cba746c2310f8db17ae177a0bb4363e4
Marc Rousavy <me@mrousavy.com>
2025-10-30 12:06:23 +0100

    Update UnsafeTransferable.swift

--- packages/react-native-nitro-modules/ios/utils/UnsafeTransferable.swift
+++ packages/react-native-nitro-modules/ios/utils/UnsafeTransferable.swift
@@ -9,10 +9,10 @@ import Foundation
 
 public protocol UnsafeTransferable: AnyObject {
   func toUnsafeRetained() -> UnsafeMutableRawPointer
-  static func fromUnsafeRetained(_ pointer: UnsafeMutableRawPointer) -> Self
+  static func fromUnsafeRetained(_ pointer: UnsafeRawPointer) -> Self
 
   func toUnsafeUnretained() -> UnsafeMutableRawPointer
-  static func fromUnsafeUnretained(_ pointer: UnsafeMutableRawPointer) -> Self
+  static func fromUnsafeUnretained(_ pointer: UnsafeRawPointer) -> Self
 }
 public extension UnsafeTransferable {
   func toUnsafeRetained() -> UnsafeMutableRawPointer {
