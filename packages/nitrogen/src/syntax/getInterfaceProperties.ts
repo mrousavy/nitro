37257e940b238b48a276f401cb5aed3e00f01a72
Marc Rousavy <me@mrousavy.com>
2025-11-08 23:58:15 -0800

    feat: Support complex/merged TS interfaces

--- packages/nitrogen/src/syntax/getInterfaceProperties.ts
+++ packages/nitrogen/src/syntax/getInterfaceProperties.ts
@@ -7,9 +7,24 @@ export function getInterfaceProperties(
   language: Language,
   interfaceType: Type<ts.ObjectType>
 ): NamedType[] {
+  const symbol = interfaceType.getAliasSymbol() ?? interfaceType.getSymbol()
+  if (symbol == null)
+    throw new Error(
+      `Interface "${interfaceType.getText()}" does not have a Symbol!`
+    )
   return interfaceType.getProperties().map((prop) => {
-    const declaration = prop.getValueDeclarationOrThrow()
-    const propType = prop.getTypeAtLocation(declaration)
+    let propType = prop.getDeclaredType()
+    if (propType.isAny() || propType.isUnknown()) {
+      // the interface is aliased/merged - we need to look into the actual declaration
+      for (const declaration of symbol.getDeclarations()) {
+        const declared = prop.getTypeAtLocation(declaration)
+        if (!declared.isAny() && !declared.isUnknown()) {
+          propType = declared
+          break
+        }
+      }
+    }
+
     const refType = createNamedType(
       language,
       prop.getName(),
