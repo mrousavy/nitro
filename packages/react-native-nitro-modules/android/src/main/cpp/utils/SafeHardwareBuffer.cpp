6c9abfcee6d78eedc5af614cc084f0912630dd1d
Marc Rousavy <me@mrousavy.com>
2025-11-12 20:51:05 +0100

    feat: Introduce `SafeHardwareBuffer`

---
+++ packages/react-native-nitro-modules/android/src/main/cpp/utils/SafeHardwareBuffer.cpp
@@ -0,0 +1,107 @@
+//
+//  SafeHardwareBuffer.cpp
+//  react-native-nitro
+//
+//  Created by Marc Rousavy on 11.07.25.
+//
+
+#pragma once
+
+#include "SafeHardwareBuffer.hpp"
+#include <android/hardware_buffer.h>
+#include <android/hardware_buffer_jni.h>
+#include <fbjni/fbjni.h>
+
+namespace margelo::nitro {
+
+using namespace facebook;
+
+SafeHardwareBuffer::SafeHardwareBuffer(const jni::alias_ref<jni::JObject>& javaHardwareBuffer) {
+#if __ANDROID_API__ >= 26
+#ifdef NITRO_DEBUG
+  if (javaHardwareBuffer == nullptr) [[unlikely]] {
+    throw std::runtime_error("Cannot create SafeHardwareBuffer - Java HardwareBuffer was null!");
+  }
+#endif
+  // Convert jobject to AHardwareBuffer* - this adds +1 retain count (we're at 1 total now)
+  _buffer = AHardwareBuffer_fromHardwareBuffer(jni::Environment::current(), javaHardwareBuffer.get());
+  _dataCached = nullptr;
+  _isLocked = false;
+#endif
+}
+
+SafeHardwareBuffer::SafeHardwareBuffer(AHardwareBuffer* /* +1 retained */ alreadyRetainedHardwareBuffer)
+    : _buffer(alreadyRetainedHardwareBuffer), _dataCached(nullptr), _isLocked(false) {}
+
+SafeHardwareBuffer::SafeHardwareBuffer(SafeHardwareBuffer&& move) noexcept
+    : _buffer(move._buffer), _dataCached(move._dataCached), _isLocked(move._isLocked) {
+  // remove the buffer pointer from the value moved into `this`
+  move._buffer = nullptr;
+  move._dataCached = nullptr;
+  move._isLocked = false;
+}
+
+SafeHardwareBuffer::SafeHardwareBuffer(const SafeHardwareBuffer& copy) : _buffer(copy._buffer), _dataCached(nullptr), _isLocked(false) {
+#if __ANDROID_API__ >= 26
+  // Add +1 retain count since we copied it now
+  AHardwareBuffer_acquire(_buffer);
+#endif
+}
+
+SafeHardwareBuffer::~SafeHardwareBuffer() {
+#if __ANDROID_API__ >= 26
+  if (_buffer != nullptr) {
+    if (_isLocked) {
+      // If it was locked, unlock it now
+      AHardwareBuffer_unlock(_buffer, nullptr);
+    }
+    // Release our 1 retain count of AHardwareBuffer*
+    AHardwareBuffer_release(_buffer);
+  }
+#endif
+}
+
+[[nodiscard]] AHardwareBuffer* SafeHardwareBuffer::buffer() const noexcept {
+  return _buffer;
+}
+
+uint8_t* SafeHardwareBuffer::data() {
+  if (_isLocked && _dataCached != nullptr) {
+    // We are still locked on the AHardwareBuffer* and have a valid buffer.
+    return _dataCached;
+  }
+  void* buffer;
+  int result = AHardwareBuffer_lock(_buffer, AHARDWAREBUFFER_USAGE_CPU_READ_MASK, -1, nullptr, &buffer);
+  if (result != 0) {
+    throw std::runtime_error("Failed to read HardwareBuffer bytes!");
+  }
+  _dataCached = static_cast<uint8_t*>(buffer);
+  _isLocked = true;
+  return _dataCached;
+}
+
+[[nodiscard]] size_t SafeHardwareBuffer::size() const {
+#if __ANDROID_API__ >= 26
+  AHardwareBuffer_Desc description;
+  AHardwareBuffer_describe(_buffer, &description);
+  size_t totalSize = description.height * description.stride;
+  return totalSize;
+#else
+  throw HardwareBuffersUnavailable();
+#endif
+}
+
+[[nodiscard]] jni::local_ref<jni::JObject> SafeHardwareBuffer::toJava() {
+#if __ANDROID_API__ >= 26
+  // 1. Convert AHardwareBuffer* to jobject - this will now be 2 retain counts
+  jobject javaObject = AHardwareBuffer_toHardwareBuffer(jni::Environment::current(), _buffer);
+  // 2. Revert to 1 retain count again
+  AHardwareBuffer_release(_buffer);
+  // 3. Return to Java
+  return jni::adopt_local(javaObject);
+#else
+  throw HardwareBuffersUnavailable();
+#endif
+}
+
+} // namespace margelo::nitro
---
+++ packages/react-native-nitro-modules/android/src/main/cpp/utils/SafeHardwareBuffer.hpp
@@ -0,0 +1,59 @@
+//
+//  SafeHardwareBuffer.hpp
+//  react-native-nitro
+//
+//  Created by Marc Rousavy on 11.07.25.
+//
+
+#pragma once
+
+#include <android/hardware_buffer.h>
+#include <android/hardware_buffer_jni.h>
+#include <fbjni/fbjni.h>
+
+namespace margelo::nitro {
+
+using namespace facebook;
+
+struct HardwareBuffersUnavailable : public std::exception {
+  [[nodiscard]] const char* what() const noexcept override {
+    return "ArrayBuffer(HardwareBuffer) requires NDK API 26 or above! (minSdk >= 26)";
+  }
+};
+
+class SafeHardwareBuffer final {
+private:
+  explicit SafeHardwareBuffer(AHardwareBuffer* /* +1 retained */ alreadyRetainedHardwareBuffer);
+  explicit SafeHardwareBuffer(const jni::alias_ref<jni::JObject>& javaHardwareBuffer);
+
+public:
+  SafeHardwareBuffer(SafeHardwareBuffer&& move) noexcept;
+  SafeHardwareBuffer(const SafeHardwareBuffer& copy);
+  ~SafeHardwareBuffer();
+
+public:
+  /**
+   * Returns the reference to this underlying `AHardwareBuffer*`.
+   * The `AHardwareBuffer*` is guaranteed to be alive as long as this
+   * `SafeHardwareBuffer` instance is alive. If you want it longer, _acquire it.
+   */
+  [[nodiscard]] AHardwareBuffer* buffer() const noexcept;
+  [[nodiscard]] uint8_t* data();
+  [[nodiscard]] size_t size() const;
+  [[nodiscard]] jni::local_ref<jni::JObject> toJava();
+
+public:
+  static SafeHardwareBuffer fromJava(const jni::alias_ref<jni::JObject>& javaHardwareBuffer) {
+    return SafeHardwareBuffer(javaHardwareBuffer);
+  }
+  static SafeHardwareBuffer wrapAlreadyRetainedAHardwareBuffer(AHardwareBuffer* /* +1 retained */ alreadyRetainedHardwareBuffer) {
+    return SafeHardwareBuffer(alreadyRetainedHardwareBuffer);
+  }
+
+private:
+  AHardwareBuffer* _buffer;
+  uint8_t* _dataCached;
+  bool _isLocked;
+};
+
+} // namespace margelo::nitro
