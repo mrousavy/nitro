1a65bda681d82dcf24a4d8ba36d195ee9fc777c8
Marc Rousavy <me@mrousavy.com>
2025-11-12 19:29:12 +0100

    fix: Make `JNull` hold a `global_ref` instead of an `alias_ref` for static field `NULL` (#1040)

--- packages/react-native-nitro-modules/android/src/main/cpp/core/JNull.hpp
+++ packages/react-native-nitro-modules/android/src/main/cpp/core/JNull.hpp
@@ -21,11 +21,12 @@ public:
   static constexpr auto kJavaDescriptor = "Lcom/margelo/nitro/core/NullType;";
 
 public:
-  static jni::alias_ref<JNull> null() {
+  static jni::global_ref<JNull> null() {
     static const auto clazz = javaClassStatic();
     static const auto nullField = clazz->getStaticField<JNull>("NULL");
     static const auto nullValue = clazz->getStaticFieldValue(nullField);
-    return nullValue;
+    static const auto globalNullValue = jni::make_global(nullValue);
+    return globalNullValue;
   }
 };
 
