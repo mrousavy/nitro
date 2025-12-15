c4318303adc7b446c7eeea5a1ae9fb17a51bfe5e
Marc Rousavy <me@mrousavy.com>
2025-12-15 15:29:04 +0100

    fix: Implement `ThreadUtils` in pure C++ on iOS

---
+++ packages/react-native-nitro-modules/ios/platform/ThreadUtils.cpp
@@ -0,0 +1,43 @@
+//
+//  ThreadUtils.cpp
+//  react-native-nitro
+//
+//  Created by Marc Rousavy on 14.07.24.
+//
+
+#include "ThreadUtils.hpp"
+#include <pthread.h>
+#include <sstream>
+#include <thread>
+#include <dispatch/dispatch.h>
+
+namespace margelo::nitro {
+
+std::string ThreadUtils::getThreadName() {
+  // Try getting DispatchQueue name
+  const char* queueName = dispatch_queue_get_label(DISPATCH_CURRENT_QUEUE_LABEL);
+  if (queueName != nullptr && queueName[0] != '\0') {
+    // We are on a DispatchQueue with a name
+    return std::string(queueName);
+  }
+  
+  // Try using pthread APIs
+  char threadName[64] = {};
+  int pthreadResult = pthread_getname_np(pthread_self(), threadName, sizeof(threadName));
+  if (pthreadResult == 0 && threadName[0] != '\0') {
+    // We have a pthread name
+    return std::string(threadName);
+  }
+
+  // Fall back to std::this_thread
+  std::stringstream stream;
+  stream << std::this_thread::get_id();
+  std::string threadId = stream.str();
+  return std::string("Thread #") + threadId;
+}
+
+void ThreadUtils::setThreadName(const std::string& name) {
+  pthread_setname_np(name.c_str());
+}
+
+} // namespace margelo::nitro
