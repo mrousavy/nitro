bdffdc77df9bd4bfbb5a29c098946a184be6420a
Szymon Chmal <szymon@chmal.it>
2025-12-22 17:22:55 +0100

    feat: re-use test suites

--- example/__tests__/nitro.harness.ts
+++ example/__tests__/nitro.harness.ts
@@ -1,7 +1,20 @@
-import { describe, it, expect } from 'react-native-harness'
+import { describe, it } from 'react-native-harness'
+import { NitroModules } from 'react-native-nitro-modules'
+import type { TestObjectCpp } from 'react-native-nitro-test'
+import { getTests } from '../src/getTests'
+import { harnessBackend } from '../src/testing/backends/harness'
 
-describe('Nitro Sanity Test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true)
-  })
+const testObject =
+  NitroModules.createHybridObject<TestObjectCpp>('TestObjectCpp')
+const tests = getTests(testObject, { backend: harnessBackend })
+
+describe('Harness tests for Nitro', () => {
+  for (const test of tests) {
+    it(test.name, async () => {
+      const result = await test.run()
+      if (result.status === 'failed') {
+        throw new Error(result.message)
+      }
+    })
+  }
 })
---
+++ example/src/testing/index.ts
@@ -0,0 +1,4 @@
+export type { AssertionBackend, JSType } from './types'
+export { State } from './State'
+export { createTestRunner, type TestRunner } from './createTestRunner'
+export { throwingBackend } from './backends/throwing'
