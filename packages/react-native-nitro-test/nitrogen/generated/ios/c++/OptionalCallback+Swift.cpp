afb45a88fbfd11c21b929c1a128fffb403576edd
Marc Rousavy <me@mrousavy.com>
2026-01-20 13:55:01 +0100

    fix: Add include for variant in struct

--- packages/react-native-nitro-test/nitrogen/generated/ios/c++/OptionalCallback+Swift.cpp
+++ packages/react-native-nitro-test/nitrogen/generated/ios/c++/OptionalCallback+Swift.cpp
@@ -9,6 +9,7 @@
 #define SWIFT_IS_IMPORTED
 #include <NitroModules/SwiftConverter.hpp>
 
+#include "Variant_______Void_Double+Swift.hpp"
 #include "OptionalCallback+Swift.hpp"
 #include "OptionalCallback.hpp"
 #include <functional>
