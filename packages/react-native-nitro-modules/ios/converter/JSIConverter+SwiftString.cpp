5d10a81736d9560ade1629699417e84585d15655
Marc Rousavy <me@mrousavy.com>
2026-01-25 23:56:58 +0100

    perf: Speed things up for string conversion :)

--- packages/react-native-nitro-modules/ios/converter/JSIConverter+SwiftString.cpp
+++ packages/react-native-nitro-modules/ios/converter/JSIConverter+SwiftString.cpp
@@ -53,8 +53,14 @@ swift::String JSIConverter<swift::String>::fromJSI(jsi::Runtime& runtime, const 
   // Concat pieces of jsi::String into swift::String - some segments may be ascii, others UTF
   swift::String string = swift::String::init();
   auto callback = [&](bool ascii, const void* data, size_t length) {
-    swift::String sequence = NitroModules::StringUtils::createString(data, length, ascii);
-    string.appendContentsOf(sequence);
+    if (ascii) {
+      swift::String sequence = NitroModules::StringUtils::createUTF8String(data, length);
+      string.appendContentsOf(sequence);
+    } else {
+      const uint16_t* utf16Bytes = static_cast<const uint16_t*>(data);
+      swift::String sequence = NitroModules::StringUtils::createUTF16String(utf16Bytes, length);
+      string.appendContentsOf(sequence);
+    }
   };
   jsString.getStringData(runtime, callback);
   // Return result
@@ -64,24 +70,24 @@ swift::String JSIConverter<swift::String>::fromJSI(jsi::Runtime& runtime, const 
 jsi::Value JSIConverter<swift::String>::toJSI(jsi::Runtime& runtime, const swift::String& string) {
   SmallUtf8Buffer<256> buffer;
   bool isAscii = true;
-  
+
   // Iterate through String's UTF8View
   swift::UTF8View view = string.getUtf8();
   swift::String_Index index = view.getStartIndex();
   swift::String_Index end = view.getEndIndex();
   const size_t endOffset = end.getEncodedOffset();
-  
+
   while (index.getEncodedOffset() != endOffset) {
     // Copy byte into our buffer
     uint8_t b = static_cast<uint8_t>(view[index]);
     if (isAscii && (b & 0x80u)) isAscii = false;
     buffer.push(b);
-    
+
     // Go to next string index
     auto next = view.indexAfter(index);
     index = next;
   }
-  
+
   if (isAscii) {
     // Ascii is the fast path and requires less storage or conversions.
     auto data = reinterpret_cast<const char*>(buffer.data());
--- packages/react-native-nitro-modules/ios/utils/StringUtils.swift
+++ packages/react-native-nitro-modules/ios/utils/StringUtils.swift
@@ -3,15 +3,30 @@
 //
 
 public final class StringUtils {
-  public static func createString(bytes: UnsafeRawPointer, length: Int, isAscii: Bool) -> String {
-    if isAscii {
-      let castBytes = bytes.assumingMemoryBound(to: CChar.self)
-      let pointer = UnsafeBufferPointer(start: castBytes, count: length)
-      return String(utf8String: pointer.baseAddress!)!
-    } else {
-      let castBytes = bytes.assumingMemoryBound(to: unichar.self)
-      let pointer = UnsafeBufferPointer(start: castBytes, count: length)
-      return String(utf16CodeUnits: pointer.baseAddress!, count: length)
+  /**
+   * Create a Swift `String` from the given UTF8/ASCII bytes (`void*`),
+   * with the given `length`.
+   *
+   * The `length` is the number of characters in the String,
+   * which (in UTF8) is also the size of the data, in bytes.
+   */
+  @inline(__always)
+  public static func createUTF8String(bytes: UnsafeRawPointer, length: Int) -> String {
+    return String(unsafeUninitializedCapacity: length) { buffer in
+      memcpy(buffer.baseAddress, bytes, length)
+      return length
     }
+  }
+
+  /**
+   * Create a Swift `String` from the given UTF16 bytes (`utf16_t*`),
+   * with the given `length`.
+   *
+   * The `length` is the number of characters in the String, which
+   * is not always the same as the data's size in bytes.
+   */
+  @inline(__always)
+  public static func createUTF16String(bytes: UnsafePointer<UInt16>, length: Int) -> String {
+    return String(utf16CodeUnits: bytes, count: length)
   }
 }
