5f143f8974db175b2be0b10e28bddcf8fbe00e77
Marc Rousavy <me@mrousavy.com>
2026-01-25 23:30:09 +0100

    perf: Make strings 2x faster

--- packages/react-native-nitro-modules/ios/utils/StringUtils.swift
+++ packages/react-native-nitro-modules/ios/utils/StringUtils.swift
@@ -5,13 +5,13 @@
 public final class StringUtils {
   public static func createString(bytes: UnsafeRawPointer, length: Int, isAscii: Bool) -> String {
     if isAscii {
-      let castBytes = bytes.assumingMemoryBound(to: UInt8.self)
+      let castBytes = bytes.assumingMemoryBound(to: CChar.self)
       let pointer = UnsafeBufferPointer(start: castBytes, count: length)
-      return String(decoding: pointer, as: UTF8.self)
+      return String(utf8String: pointer.baseAddress!)!
     } else {
-      let castBytes = bytes.assumingMemoryBound(to: UInt16.self)
+      let castBytes = bytes.assumingMemoryBound(to: unichar.self)
       let pointer = UnsafeBufferPointer(start: castBytes, count: length)
-      return String(decoding: pointer, as: UTF16.self)
+      return String(utf16CodeUnits: pointer.baseAddress!, count: length)
     }
   }
 }
