69bf5841f4e4ce755f91f925e0700d9bcd730672
Marc Rousavy <me@mrousavy.com>
2026-03-03 17:53:29 +0100

    fix: Replace `Bool(fromCxx:)` with `use_count()` to fix Swift build error

--- packages/react-native-nitro-test/nitrogen/generated/ios/swift/HybridRecyclableTestViewSpec_cxx.swift
+++ packages/react-native-nitro-test/nitrogen/generated/ios/swift/HybridRecyclableTestViewSpec_cxx.swift
@@ -75,7 +75,7 @@ open class HybridRecyclableTestViewSpec_cxx {
    */
   public func getCxxPart() -> bridge.std__shared_ptr_HybridRecyclableTestViewSpec_ {
     let cachedCxxPart = self.__cxxPart.lock()
-    if Bool(fromCxx: cachedCxxPart) {
+    if cachedCxxPart.use_count() > 0 {
       return cachedCxxPart
     } else {
       let newCxxPart = bridge.create_std__shared_ptr_HybridRecyclableTestViewSpec_(self.toUnsafe())
