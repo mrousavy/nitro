2c0da09f11144c8215f4f795eb062f06c635b5fb
Marc Rousavy <me@mrousavy.com>
2026-03-11 15:42:33 +0100

    chore: release 0.35.1

--- packages/react-native-nitro-modules/cpp/utils/NitroDefines.hpp
+++ packages/react-native-nitro-modules/cpp/utils/NitroDefines.hpp
@@ -9,7 +9,7 @@
 #define NitroDefines_h
 
 // Sets the version of the native Nitro core library
-#define NITRO_VERSION "0.35.1-beta.0"
+#define NITRO_VERSION "0.35.1-beta.2"
 
 // Sets whether to use debug or optimized production build flags
 #ifdef DEBUG
