001395a228c8e3b11661aa9e0e5f12a6ca671718
Marc Rousavy <me@mrousavy.com>
2026-04-28 16:55:42 +0200

    format

--- packages/react-native-nitro-test/cpp/HybridTestObjectCpp.cpp
+++ packages/react-native-nitro-test/cpp/HybridTestObjectCpp.cpp
@@ -216,8 +216,7 @@ std::vector<Person> HybridTestObjectCpp::bounceStructs(const std::vector<Person>
   return array;
 }
 
-std::vector<std::vector<Person>>
-HybridTestObjectCpp::bounceNestedArray(const std::vector<std::vector<Person>>& array) {
+std::vector<std::vector<Person>> HybridTestObjectCpp::bounceNestedArray(const std::vector<std::vector<Person>>& array) {
   return array;
 }
 
