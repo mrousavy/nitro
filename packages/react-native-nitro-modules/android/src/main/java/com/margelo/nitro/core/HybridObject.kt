0c3c936e7ffc8993b1375e8fea854da46c6fd8a2
Marc Rousavy <me@mrousavy.com>
2026-05-19 13:25:55 +0200

    chore: test no `resetNative()` to see if it fails

--- packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/core/HybridObject.kt
+++ packages/react-native-nitro-modules/android/src/main/java/com/margelo/nitro/core/HybridObject.kt
@@ -102,7 +102,7 @@ abstract class HybridObject {
     }
 
     fun destroy() {
-      mHybridData.resetNative()
+      // noop for testing
     }
 
     protected open external fun initHybrid(): HybridData
