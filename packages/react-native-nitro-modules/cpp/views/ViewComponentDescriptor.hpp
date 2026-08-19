09d8c092bd3eaf311f7d45380fdfcc5b63caf59d
Marc Rousavy <me@mrousavy.com>
2026-08-19 12:56:22 +0200

    feat: Add base files

---
+++ packages/react-native-nitro-modules/cpp/views/ViewComponentDescriptor.hpp
@@ -0,0 +1,13 @@
+//
+// Created by Marc Rousavy on 19.08.26.
+//
+
+#pragma once
+
+#include <react/renderer/core/ConcreteComponentDescriptor.h>
+
+namespace margelo::nitro {
+
+using namespace facebook;
+
+} // namespace margelo::nitro
---
+++ packages/react-native-nitro-modules/cpp/views/ViewPropsHolderState.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Marc Rousavy on 19.08.26.
+//
+
+#pragma once
+
+#include "ViewPropsHolderState.hpp"
+
+namespace margelo::nitro {
+
+using namespace facebook;
+
+} // namespace margelo::nitro
