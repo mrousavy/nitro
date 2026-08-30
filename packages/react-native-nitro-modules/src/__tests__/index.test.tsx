e63f0d2e5c1784ce1db496f3db2c456e3cd9769c
Oskar Eichler <oskar@songstats.com>
2026-08-30 08:59:36 +0200

    fix: Handle primitive HybridObject instanceof

--- packages/react-native-nitro-modules/src/__tests__/index.test.tsx
+++ packages/react-native-nitro-modules/src/__tests__/index.test.tsx
@@ -1,1 +1,22 @@
-it.todo('write a test')
+const mockHybridPrototype = {}
+const mockCreateHybridObject = jest.fn(() => Object.create(mockHybridPrototype))
+
+jest.mock('../NitroModules', () => ({
+  NitroModules: {
+    createHybridObject: mockCreateHybridObject,
+  },
+}))
+
+const { getHybridObjectConstructor } = require('../getHybridObjectConstructor')
+
+describe('getHybridObjectConstructor', () => {
+  it.each([null, undefined, true, 42, 'value', Symbol('value'), 1n])(
+    'returns false for non-object value %p',
+    (value) => {
+      const HybridObject = getHybridObjectConstructor('TestObject')
+
+      expect((value as any) instanceof HybridObject).toBe(false)
+      expect(mockCreateHybridObject).not.toHaveBeenCalled()
+    }
+  )
+})
