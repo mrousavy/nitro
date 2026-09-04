8048da4a41c215e105710093b6d88dd8ef610e80
Marc Rousavy <me@mrousavy.com>
2026-09-04 12:11:42 +0300

    chore: move example app into apps/example

--- apps/example/metro.config.js
+++ apps/example/metro.config.js
@@ -1,8 +1,8 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
 const path = require('path')
-const pak = require('../package.json')
+const pak = require('../../package.json')
 
-const root = path.resolve(__dirname, '..')
+const root = path.resolve(__dirname, '../..')
 const modules = Object.keys({ ...pak.peerDependencies })
 
 /**
